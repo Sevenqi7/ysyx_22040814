f48cca49fbc94bc3c0793f0c73611190424ad6a5
Seven7 <734633635@qq.com>
2023-03-07 13:42:18 +0800

    try nvboard

--- npc/obj_dir/Vtop.mk
+++ npc/obj_dir/Vtop.mk
@@ -45,7 +45,7 @@ VM_USER_CLASSES = \
 
 # User .cpp directories (from .cpp's on Verilator command line)
 VM_USER_DIR = \
-	./csrc \
+	/home/seven7/Documents/学业/一生一芯/ysyx-workbench/npc/csrc \
 
 
 ### Default rules...
@@ -57,7 +57,7 @@ include $(VERILATOR_ROOT)/include/verilated.mk
 ### Executable rules... (from --exe)
 VPATH += $(VM_USER_DIR)
 
-main.o: ./csrc/main.cpp
+main.o: /home/seven7/Documents/学业/一生一芯/ysyx-workbench/npc/csrc/main.cpp
 	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST) -c -o $@ $<
 
 ### Link rules... (from --exe)
