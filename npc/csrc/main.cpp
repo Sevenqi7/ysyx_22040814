3f980ac7de7a52071da641dbefed1cf9e4720420
Seven7 <734633635@qq.com>
2023-03-07 19:18:01 +0800

    complete

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,48 +1,31 @@
-#include <memory>
-#include "Vtop.h"
-#include <verilated.h>
+#include <nvboard.h>
+#include <Vtop.h>
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
+static Vtop dut;
 
-int main(int argc, char** argv, char** env) {
-  printf("Hello, ysyx!\n");
-  int times = 10;
+void nvboard_bind_all_pins(Vtop* top);
 
-    // Using unique_ptr is similar to
-    // "VerilatedContext* contextp = new VerilatedContext" then deleting at end.
-  const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
-
-    // Set debug level, 0 is off, 9 is highest presently used
-    // May be overridden by commandArgs argument parsing
-  contextp->debug(0);
-
-    // Randomization reset policy
-    // May be overridden by commandArgs argument parsing
-  contextp->randReset(2);
-
-    // Verilator must compute traced signals
-  contextp->traceEverOn(true);
-
-    // Pass arguments so Verilated code can see them, e.g. $value$plusargs
-    // This needs to be called before you create any model
-  contextp->commandArgs(argc, argv);
-
-    // Construct the Verilated model, from Vtop.h generated from Verilating "top.v".
-    // Using unique_ptr is similar to "Vtop* top = new Vtop" then deleting at end.
-    // "TOP" will be the hierarchical name of the module.
-  const std::unique_ptr<Vtop> top{new Vtop{contextp.get(), "TOP"}};
-
-  while (times--) {
-      int a = rand() & 1;
-      int b = rand() & 1;
-      top->a = a;
-      top->b = b;
-      top->eval();
-      printf("a = %d, b = %d, f = %d\n", a, b, top->f);
-//       contextp->timeInc(1);
-      assert(top->f == (a ^ b));
-  }
-  return 0;
+static void single_cycle() {
+  dut.clk = 0; dut.eval();
+  dut.clk = 1; dut.eval();
 }
+
+static void reset(int n) {
+  dut.rst = 1;
+  while (n -- > 0) single_cycle();
+  dut.rst = 0;
+}
+
+int main() {
+  nvboard_bind_all_pins(&dut);
+  nvboard_init();
+
+  reset(10);
+
+  while(1) {
+    nvboard_update();
+    single_cycle();
+  }
+  nvboard_quit();
+}
+
--- npc/vsrc/switch.v
+++ npc/vsrc/switch.v
@@ -1,25 +1,9 @@
-module top(
-  input clk,
-  input rst,
-  input [1:0] sw,
-  output [15:0] ledr
+module switch(
+  input a,
+  input b,
+  output f
 );
-  wire led_sw;
-
-  switch switch1(
-    .a(sw[0]),
-    .b(sw[1]),
-    .f(led_sw)
-  );
-
-  led led1(
-    .clk(clk),
-    .rst(rst),
-    .sw(led_sw),
-    .ledr(ledr)
-  );
-
-
+  assign f = a ^ b;
 
 // initial begin
 //     if ($test$plusargs("trace") != 0) begin
