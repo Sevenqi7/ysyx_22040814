b59a69e32c90f734097c2fc3007ae57cbe4c4646
Seven7 <734633635@qq.com>
2023-03-11 00:32:19 +0800

    finish

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,60 +1,97 @@
 module top(
-  input  [10:0] io_sw,
-  output [2:0]  io_led,
-  output [6:0]  io_seg_0,
-  output [6:0]  io_seg_1,
-  output [6:0]  io_seg_2,
-  output [6:0]  io_seg_3,
-  output [6:0]  io_seg_4,
-  output [6:0]  io_seg_5,
-  output [6:0]  io_seg_6,
-  output [6:0]  io_seg_7
+  input        clock,
+  input        reset,
+  input  [7:0] io_sw,
+  output [6:0] io_seg_0,
+  output [6:0] io_seg_1,
+  output [6:0] io_seg_2,
+  output [6:0] io_seg_3,
+  output [6:0] io_seg_4,
+  output [6:0] io_seg_5,
+  output [6:0] io_seg_6,
+  output [6:0] io_seg_7,
+  input        io_sw_ctrl
 );
-  wire [3:0] myalu_io_A; // @[top.scala 13:23]
-  wire [3:0] myalu_io_B; // @[top.scala 13:23]
-  wire [3:0] myalu_io_out; // @[top.scala 13:23]
-  wire [2:0] myalu_io_op_type; // @[top.scala 13:23]
-  wire  myalu_io_zero_flag; // @[top.scala 13:23]
-  wire  myalu_io_carry_flag; // @[top.scala 13:23]
-  wire  myalu_io_overflow_flag; // @[top.scala 13:23]
-  wire [2:0] seg0decoder_io_in; // @[top.scala 19:29]
-  wire [6:0] seg0decoder_io_out; // @[top.scala 19:29]
-  wire [2:0] seg1decoder_io_in; // @[top.scala 20:29]
-  wire [6:0] seg1decoder_io_out; // @[top.scala 20:29]
-  wire [3:0] _seg0decoder_io_in_T = myalu_io_out % 4'ha; // @[top.scala 21:39]
-  wire [3:0] _seg1decoder_io_in_T = myalu_io_out / 4'ha; // @[top.scala 22:39]
-  wire [1:0] _io_led_T = {myalu_io_overflow_flag,myalu_io_carry_flag}; // @[Cat.scala 33:92]
-  MyALU myalu ( // @[top.scala 13:23]
-    .io_A(myalu_io_A),
-    .io_B(myalu_io_B),
-    .io_out(myalu_io_out),
-    .io_op_type(myalu_io_op_type),
-    .io_zero_flag(myalu_io_zero_flag),
-    .io_carry_flag(myalu_io_carry_flag),
-    .io_overflow_flag(myalu_io_overflow_flag)
-  );
-  DE10Decoder seg0decoder ( // @[top.scala 19:29]
+`ifdef RANDOMIZE_REG_INIT
+  reg [31:0] _RAND_0;
+`endif // RANDOMIZE_REG_INIT
+  wire [7:0] seg0decoder_io_in; // @[top.scala 14:29]
+  wire [6:0] seg0decoder_io_out; // @[top.scala 14:29]
+  wire [7:0] seg1decoder_io_in; // @[top.scala 15:29]
+  wire [6:0] seg1decoder_io_out; // @[top.scala 15:29]
+  reg [7:0] shiftReg; // @[top.scala 13:27]
+  wire  newVal = shiftReg[0] ^ shiftReg[2] ^ shiftReg[3] ^ shiftReg[4]; // @[top.scala 16:58]
+  wire [7:0] _shiftReg_T_1 = {newVal,shiftReg[7:1]}; // @[Cat.scala 33:92]
+  DE10HexDecoder seg0decoder ( // @[top.scala 14:29]
     .io_in(seg0decoder_io_in),
     .io_out(seg0decoder_io_out)
   );
-  DE10Decoder seg1decoder ( // @[top.scala 20:29]
+  DE10HexDecoder seg1decoder ( // @[top.scala 15:29]
     .io_in(seg1decoder_io_in),
     .io_out(seg1decoder_io_out)
   );
-  assign io_led = {_io_led_T,myalu_io_zero_flag}; // @[Cat.scala 33:92]
-  assign io_seg_0 = seg0decoder_io_out; // @[top.scala 24:15]
-  assign io_seg_1 = seg1decoder_io_out; // @[top.scala 25:15]
-  assign myalu_io_A = io_sw[3:0]; // @[top.scala 14:24]
-  assign myalu_io_B = io_sw[7:4]; // @[top.scala 15:24]
-  assign myalu_io_op_type = io_sw[10:8]; // @[top.scala 16:30]
-  assign seg0decoder_io_in = _seg0decoder_io_in_T[2:0]; // @[top.scala 21:23]
-  assign seg1decoder_io_in = _seg1decoder_io_in_T[2:0]; // @[top.scala 22:23]
-
-  assign io_seg_2 = 7'b1111111; // @[top.scala 25:15]
-  assign io_seg_3 = 7'b1111111; // @[top.scala 25:15]
-  assign io_seg_4 = 7'b1111111; // @[top.scala 25:15]
-  assign io_seg_5 = 7'b1111111; // @[top.scala 25:15]
-  assign io_seg_6 = 7'b1111111; // @[top.scala 25:15]
-  assign io_seg_7 = 7'b1111111; // @[top.scala 25:15]
-
+  assign io_seg_0 = seg0decoder_io_out; // @[top.scala 26:15]
+  assign io_seg_1 = seg1decoder_io_out; // @[top.scala 27:15]
+  assign io_seg_2 = 7'h7f; // @[top.scala 29:19]
+  assign io_seg_3 = 7'h7f; // @[top.scala 29:19]
+  assign io_seg_4 = 7'h7f; // @[top.scala 29:19]
+  assign io_seg_5 = 7'h7f; // @[top.scala 29:19]
+  assign io_seg_6 = 7'h7f; // @[top.scala 29:19]
+  assign io_seg_7 = 7'h7f; // @[top.scala 29:19]
+  assign seg0decoder_io_in = {{4'd0}, shiftReg[3:0]}; // @[top.scala 24:23]
+  assign seg1decoder_io_in = {{4'd0}, shiftReg[7:4]}; // @[top.scala 25:23]
+  always @(posedge clock) begin
+    if (reset) begin // @[top.scala 13:27]
+      shiftReg <= 8'h0; // @[top.scala 13:27]
+    end else if (~io_sw_ctrl) begin // @[top.scala 17:29]
+      shiftReg <= _shiftReg_T_1; // @[top.scala 18:18]
+    end else begin
+      shiftReg <= io_sw; // @[top.scala 21:18]
+    end
+  end
+// Register and memory initialization
+`ifdef RANDOMIZE_GARBAGE_ASSIGN
+`define RANDOMIZE
+`endif
+`ifdef RANDOMIZE_INVALID_ASSIGN
+`define RANDOMIZE
+`endif
+`ifdef RANDOMIZE_REG_INIT
+`define RANDOMIZE
+`endif
+`ifdef RANDOMIZE_MEM_INIT
+`define RANDOMIZE
+`endif
+`ifndef RANDOM
+`define RANDOM $random
+`endif
+`ifdef RANDOMIZE_MEM_INIT
+  integer initvar;
+`endif
+`ifndef SYNTHESIS
+`ifdef FIRRTL_BEFORE_INITIAL
+`FIRRTL_BEFORE_INITIAL
+`endif
+initial begin
+  `ifdef RANDOMIZE
+    `ifdef INIT_RANDOM
+      `INIT_RANDOM
+    `endif
+    `ifndef VERILATOR
+      `ifdef RANDOMIZE_DELAY
+        #`RANDOMIZE_DELAY begin end
+      `else
+        #0.002 begin end
+      `endif
+    `endif
+`ifdef RANDOMIZE_REG_INIT
+  _RAND_0 = {1{`RANDOM}};
+  shiftReg = _RAND_0[7:0];
+`endif // RANDOMIZE_REG_INIT
+  `endif // RANDOMIZE
+end // initial
+`ifdef FIRRTL_AFTER_INITIAL
+`FIRRTL_AFTER_INITIAL
+`endif
+`endif // SYNTHESIS
 endmodule
