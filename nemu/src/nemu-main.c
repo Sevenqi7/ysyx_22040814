48b7030ccb16532a20daa72c99ff66bd099d325c
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-12 18:08:12 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-67-generic #74-Ubuntu SMP Wed Feb 22 14:14:39 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:08:12 up  3:06,  6 users,  load average: 1.36, 1.09, 0.97

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -34,7 +34,7 @@ int main(int argc, char *argv[]) {
   char str[65535];
   int i=0;
   bool s;
-  word_t a = 1-3;
+  word_t a = 1;
   printf("\n%lu", a);
   assert(0);
   // assert(expr("((((   7))) /(5   ) +(  9 *  0    +3    -     1   +   7    / (  9 )) + (   2   *    4   ))", &s) == 2);
