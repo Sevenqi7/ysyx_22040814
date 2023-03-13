b22005d97ea9224b5af8b58101fd86c56d5f8ece
Seven7 <734633635@qq.com>
2023-03-13 21:10:09 +0800

    PA0 Finished

--- npc/vsrc/ps2_keyboard.v
+++ npc/vsrc/ps2_keyboard.v
@@ -1,46 +1,48 @@
-module ps2_keyboard(clk,clrn,ps2_clk,ps2_data,data,
-                    ready,nextdata_n,overflow);
-    input clk,clrn,ps2_clk,ps2_data;
-    input nextdata_n;
+module ps2_keyboard(clk,resetn,ps2_clk,ps2_data, data, state, push_cnt);
+    input clk,resetn,ps2_clk,ps2_data;
     output [7:0] data;
-    output reg ready;
-    output reg overflow;     // fifo overflow
-    // internal signal, for test
-    reg [9:0] buffer;        // ps2_data bits
-    reg [7:0] fifo[7:0];     // data fifo
-    reg [2:0] w_ptr,r_ptr;   // fifo write and read pointers
-    reg [3:0] count;  // count ps2_data bits
-    // detect falling edge of ps2_clk
-    reg [2:0] ps2_clk_sync;
+    output reg [1:0] state = 2'b00;
+    output reg [7:0] push_cnt = 8'b0;
 
+    reg [9:0] buffer;        // ps2_data bits
+    reg [3:0] count;  // count ps2_data bits
+    reg [2:0] ps2_clk_sync;
+    reg [7:0] data_buf;
+
+    assign data = data_buf[7:0];
     always @(posedge clk) begin
         ps2_clk_sync <=  {ps2_clk_sync[1:0],ps2_clk};
     end
 
     wire sampling = ps2_clk_sync[2] & ~ps2_clk_sync[1];
-
     always @(posedge clk) begin
-        if (clrn == 0) begin // reset
-            count <= 0; w_ptr <= 0; r_ptr <= 0; overflow <= 0; ready<= 0;
+        if (resetn == 0) begin // reset
+            count <= 0;
         end
         else begin
-            if ( ready ) begin // read to output next data
-                if(nextdata_n == 1'b0) //read next data
-                begin
-                    r_ptr <= r_ptr + 3'b1;
-                    if(w_ptr==(r_ptr+1'b1)) //empty
-                        ready <= 1'b0;
-                end
-            end
             if (sampling) begin
               if (count == 4'd10) begin
                 if ((buffer[0] == 0) &&  // start bit
                     (ps2_data)       &&  // stop bit
                     (^buffer[9:1])) begin      // odd  parity
-                    fifo[w_ptr] <= buffer[8:1];  // kbd scan code
-                    w_ptr <= w_ptr+3'b1;
-                    ready <= 1'b1;
-                    overflow <= overflow | (r_ptr == (w_ptr + 3'b1));
+                    if(state == 2'b00) begin
+                        data_buf <= buffer[8:1];
+                        state <= 2'b01;
+                        push_cnt <= push_cnt + 1'b1;
+                        $display("push_cnt %x", push_cnt[7:0]);
+                    end
+                    else if(state == 2'b01) begin
+                        if(buffer[8:1] == 8'hf0)
+                            state <= 2'b10;
+                    end
+                    else if(state == 2'b10) begin
+                        if(buffer[8:1] == data_buf[7:0])
+                            state <= 2'b00;
+                    end
+          
+                    $display("receive %x", buffer[8:1]);
+                    // $display("receive %x", data_buf[7:0]);
+
                 end
                 count <= 0;     // for next
               end else begin
@@ -50,6 +52,5 @@ module ps2_keyboard(clk,clrn,ps2_clk,ps2_data,data,
             end
         end
     end
-    assign data = fifo[r_ptr]; //always set output data
 
 endmodule
