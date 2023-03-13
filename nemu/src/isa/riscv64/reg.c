5800a9d39148b4d073af931b1d5dd06ab2490fe8
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-13 17:22:42 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-67-generic #74-Ubuntu SMP Wed Feb 22 14:14:39 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:22:42 up  1:29,  6 users,  load average: 0.81, 0.91, 1.24

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -36,8 +36,8 @@ word_t isa_reg_str2val(const char *s, bool *success) {
   for(i=1;i<32;i++)
     if(!strcmp(s+1, regs[i])) 
         break;
-  if(!strcmp(s, regs[0])) i = 0;
-  if(!strcmp(s, "$pc")) return cpu.pc;
+  if(!strcmp(s, regs[0])) {*success = true; return 0;}
+  if(!strcmp(s, "$pc")) {*success = true; return cpu.pc;}
   if(i < 32)
   {
       *success = true;
