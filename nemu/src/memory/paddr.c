39c300dfda45e85ba20c4655493d781865b646c4
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-15 17:52:18 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-67-generic #74-Ubuntu SMP Wed Feb 22 14:14:39 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:52:18 up  2:10,  4 users,  load average: 0.67, 0.83, 0.87

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -57,19 +57,19 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
-  word_t read_data = 0;
-  if (likely(in_pmem(addr))) read_data = pmem_read(addr, len);
-  IFDEF(CONFIG_DEVICE, read_data = mmio_read(addr, len));
-  out_of_bound(addr);
   #ifdef CONFIG_MTRACE
-      printf("      Read data:%lx from %x with %d bytes\n", read_data, addr, len);
+      printf("      Read %d bytes from %x\n", len, addr);
   #endif
-  return read_data;
+  // word_t read_data = 0;
+  if (likely(in_pmem(addr))) return pmem_read(addr, len);
+  IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
+  out_of_bound(addr);
+  return 0;
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
   #ifdef CONFIG_MTRACE
-      printf("      Write data:%lx to %x with %d bytes\n", data, addr ,len);
+      printf("      Write data:%lx to %x with bytes num %d\n", data, addr ,len);
   #endif
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
