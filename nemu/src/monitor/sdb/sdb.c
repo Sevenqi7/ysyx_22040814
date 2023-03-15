d439c12347d5ab4f750876c630ff44948f087049
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-15 22:05:17 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-67-generic #74-Ubuntu SMP Wed Feb 22 14:14:39 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:05:17 up  6:23,  5 users,  load average: 1.43, 1.16, 0.98

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -261,10 +261,8 @@ void sdb_get_symbol_list(char *elf_path)
       lseek(fd, ehdr.e_shoff + ehdr.e_shentsize * i, SEEK_SET);
       if(read(fd, &shdr, sizeof(Elf64_Shdr)) != sizeof(Elf64_Shdr))
           return;
-      if(shdr.sh_type == SHT_STRTAB && !strcmp(".strtab", shstrtab + shdr.sh_name)){
-          assert(0);
+      if(shdr.sh_type == SHT_STRTAB && !strcmp(".strtab", shstrtab + shdr.sh_name))
           break;
-      }
   }
 
   char *strtab = malloc(shdr.sh_size);
