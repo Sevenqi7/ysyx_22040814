38d5abb69de0492fd50a5e451c4fae6902942ad4
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-15 11:56:54 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-67-generic #74-Ubuntu SMP Wed Feb 22 14:14:39 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:56:54 up  1:36,  4 users,  load average: 1.22, 1.11, 1.00

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,16 +5,75 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+static int itoa(int x, char str[]);
+
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+  va_list args;
+  va_start(args, fmt);
+  int ret = 0;
+  for(int i=0;fmt[i];i++)
+  {
+      if(fmt[i] == '%')
+      {
+          i++;
+          switch(fmt[i])
+          {
+              case 'c':
+                  char ch = va_arg(args, int);
+                  putch(ch), ret++; 
+                  break;
+              case 's': 
+                  for(char *s = va_arg(args, char *);*s;s++) putch(*s), ret++;
+                  break;
+              default:
+                  panic("Unimplemented argument!");
+          }
+      }
+  } 
+  va_end(args);
+  return ret;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  int len = 0;
+  char *p = out;
+  for(int i=0;fmt[i];i++)
+  {
+      if(fmt[i] == '%')
+      {
+          i++;
+          switch(fmt[i])
+          {
+              case 's':
+                  char *str = va_arg(ap, char*);
+                  for(int j=0;str[j];j++) *p++ = str[j], len++;
+                  break;
+              case 'd':
+                  char num[11];       //max of int is 2147483647
+                  int x = va_arg(ap, int);
+                  itoa(x, num);
+                  for(int j=0;num[j];j++) *p++ = num[j], len++;
+                  break;
+              default:
+                  panic("Unimplemented Argument in vsprintf!\n");
+          }
+      }
+      else
+      {
+          *p++ = fmt[i];
+          len++;
+      }
+  }
+  
+  return len;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list args;
+  va_start(args, fmt);
+  int ret = vsprintf(out, fmt, args);
+  va_end(args);
+  return ret;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
@@ -23,6 +82,34 @@ int snprintf(char *out, size_t n, const char *fmt, ...) {
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
   panic("Not implemented");
+}
+
+static int itoa(int x, char str[])
+{
+    char *p = str;
+    int sign = x;
+    if(x < 0)
+    {
+        *p++ = '-';
+        x = -x;   
+    }
+
+    for(int i=0;x;i++)
+    {
+        *p++ = x % 10 + '0';
+        x /= 10;
+    }
+    *p-- = '\0';
+    char *q = str;
+    if(sign == -1) q++;
+    for(q=str;q<p;p++,q--)
+    {
+        char temp = *p;
+        *p = *q;
+        *q = temp;
+    }
+
+    return strlen(str);
 }
 
 #endif
