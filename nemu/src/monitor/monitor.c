fbed0e97bd3f7daf1dc7b847cd30411157824059
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-15 20:02:43 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-67-generic #74-Ubuntu SMP Wed Feb 22 14:14:39 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:02:43 up  4:20,  5 users,  load average: 0.77, 0.92, 0.93

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -40,6 +40,7 @@ static void welcome() {
 #include <getopt.h>
 
 void sdb_set_batch_mode();
+void sdb_get_symbol_list();
 
 static char *log_file = NULL;
 static char *diff_so_file = NULL;
@@ -75,15 +76,17 @@ static int parse_args(int argc, char *argv[]) {
     {"diff"     , required_argument, NULL, 'd'},
     {"port"     , required_argument, NULL, 'p'},
     {"help"     , no_argument      , NULL, 'h'},
+    {"elf"      , required_argument, NULL, 'e'},
     {0          , 0                , NULL,  0 },
   };
   int o;
-  while ( (o = getopt_long(argc, argv, "-bhl:d:p:", table, NULL)) != -1) {
+  while ( (o = getopt_long(argc, argv, "-bhl:d:p:e:", table, NULL)) != -1) {
     switch (o) {
       case 'b': sdb_set_batch_mode(); break;
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
       case 'l': log_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
+      case 'e': sdb_get_symbol_list(optarg);
       case 1: img_file = optarg; return 0;
       default:
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
@@ -91,6 +94,7 @@ static int parse_args(int argc, char *argv[]) {
         printf("\t-l,--log=FILE           output log to FILE\n");
         printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
         printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
+        printf("\t-e,--elf=ELF_FILE       read ELF for ftrace\n");
         printf("\n");
         exit(0);
     }
--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -18,10 +18,19 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include <memory/paddr.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <elf.h>
 #include "sdb.h"
 
 
 static int is_batch_mode = false;
+static int f_info_num = 0;
+static struct function_info
+{
+    uint32_t f_addr;
+    char f_name[128];
+}f_info[32];
 
 void init_regex();
 void init_wp_pool();
@@ -216,6 +225,69 @@ static int cmd_help(char *args) {
 
 void sdb_set_batch_mode() {
   is_batch_mode = true;
+}
+
+void sdb_get_symbol_list(char *elf_path)
+{
+  int fd = open(elf_path, O_RDONLY, 0);
+  if(fd == -1)
+  {
+    printf("Failed to open elf file!\n");
+    return ;
+  }
+  Elf64_Ehdr ehdr;
+  if(read(fd, &ehdr, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr))
+  {
+      printf("Failed to read Elf Header\n");
+      return ;
+  }
+  lseek(fd, ehdr.e_shoff + ehdr.e_shentsize * ehdr.e_shstrndx, SEEK_SET);
+  Elf64_Shdr shdr;
+  if(read(fd, &shdr, sizeof(shdr)) != sizeof(Elf64_Shdr))
+  {
+      printf("Failed to read section header of string table!\n");
+      return ;
+  }
+  char *strtab = malloc(shdr.sh_size);
+  lseek(fd, shdr.sh_offset, SEEK_SET);
+  if(read(fd, strtab, shdr.sh_size) != shdr.sh_size)
+  {
+      printf("Failed to read string table!\n");
+      return ;
+  }
+  lseek(fd, ehdr.e_shoff, SEEK_SET);
+  for(int i=0;i<ehdr.e_shnum;i++)
+  {
+      if(read(fd, &shdr, sizeof(Elf64_Shdr)) != sizeof(Elf64_Shdr))
+      {
+          printf("Failed to load section %d!\n", i);
+          return ;
+      }
+      if(shdr.sh_type == SHT_SYMTAB)
+      {
+          Elf64_Sym sym;
+          lseek(fd, shdr.sh_offset, SEEK_SET);
+          int sym_num = shdr.sh_size / sizeof(Elf64_Sym);
+          for(int j=0;j<sym_num;j++)
+          {
+              if(read(fd, &sym, sizeof(Elf64_Sym)) != sizeof(Elf64_Sym))
+              {
+                  printf("Failed to read symbol info!\n");
+                  return ;
+              }
+              if(sym.st_info == STT_FUNC)
+              {
+                  char *func_name = strtab + sym.st_name;
+                  memcpy(f_info[f_info_num].f_name, func_name, strlen(func_name)+1);
+                  f_info[f_info_num].f_addr = sym.st_value;
+              }
+          }
+          
+      }
+  }
+  
+  Log("Funciton symbol load success.");
+
 }
 
 void sdb_mainloop() {
