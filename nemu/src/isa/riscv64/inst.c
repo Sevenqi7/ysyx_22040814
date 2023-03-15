4af1d656c0d7da4a764c27134f064af19c10d0d5
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-15 21:02:00 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-67-generic #74-Ubuntu SMP Wed Feb 22 14:14:39 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:02:00 up  5:20,  5 users,  load average: 0.94, 0.93, 0.98

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -41,7 +41,7 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
   int rs2 = BITS(i, 24, 20);
   *rd     = BITS(i, 11, 7);
   immB();
-  Log("immB:0x%lx rs1:%d. rs2:%d rd:%d", *imm, rs1, rs2, *rd);
+  // Log("immB:0x%lx rs1:%d. rs2:%d rd:%d", *imm, rs1, rs2, *rd);
   switch (type) {
     case TYPE_I: src1R();          immI(); break;
     case TYPE_U:                   immU(); break;
