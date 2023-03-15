91d5c2573054f334b7519059f8892e8bf8e5d90f
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-16 00:57:15 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-67-generic #74-Ubuntu SMP Wed Feb 22 14:14:39 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:57:15 up  9:15,  5 users,  load average: 1.71, 1.37, 1.16

--- nemu/src/monitor/ftrace.c
+++ nemu/src/monitor/ftrace.c
@@ -48,7 +48,7 @@ void ftrace_check_jal(vaddr_t jump_addr, vaddr_t ret_addr, int rs1, int rd)
         if(f_info[i].f_addr == jump_addr)
         {
             f_trace_buf.function[f_trace_buf.f_trace_end] = f_info[i];
-            f_trace_buf.ret_addr[f_trace_buf.f_trace_end] = ret_addr;
+            // f_trace_buf.ret_addr[f_trace_buf.f_trace_end] = ret_addr;
             f_trace_buf.is_ret[f_trace_buf.f_trace_end] = false;
             Log("jump to 0x%lx(%s)", f_trace_buf.function[f_trace_buf.f_trace_end].f_addr, f_trace_buf.function[f_trace_buf.f_trace_end].f_name);
 
@@ -64,9 +64,9 @@ void ftrace_check_jalr(vaddr_t jump_addr, vaddr_t ret_addr, int rd)
     {
         if(f_info[i].f_addr == jump_addr)
         {
-            // f_trace_buf.function[f_trace_buf.f_trace_end] = f_info[i];
-            // f_trace_buf.ret_addr[f_trace_buf.f_trace_end] = ret_addr;
-            // f_trace_buf.is_ret[f_trace_buf.f_trace_end] = false;
+            f_trace_buf.function[f_trace_buf.f_trace_end] = f_info[i];
+            f_trace_buf.ret_addr[f_trace_buf.f_trace_end] = ret_addr;
+            f_trace_buf.is_ret[f_trace_buf.f_trace_end] = false;
             Log("jump to %lx", f_trace_buf.function[f_trace_buf.f_trace_end].f_addr);
             f_trace_buf.f_trace_end++;
             return ;
