0f303a8f776fd40b7927278d6ad44aeb4e9d6413
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-16 00:56:25 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-67-generic #74-Ubuntu SMP Wed Feb 22 14:14:39 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:56:25 up  9:14,  5 users,  load average: 1.73, 1.28, 1.12

--- nemu/src/monitor/ftrace.c
+++ nemu/src/monitor/ftrace.c
@@ -9,7 +9,7 @@
 #include <elf.h>
 
 #define MAX_FTRACE_INFO_SIZE 32
-#define MAX_FTRACE_STACK_SIZE 64
+#define MAX_FTRACE_STACK_SIZE 96
 
 static int f_info_num = 0;
 static struct function_info
@@ -47,8 +47,8 @@ void ftrace_check_jal(vaddr_t jump_addr, vaddr_t ret_addr, int rs1, int rd)
     {
         if(f_info[i].f_addr == jump_addr)
         {
-            // f_trace_buf.function[f_trace_buf.f_trace_end] = f_info[i];
-            // f_trace_buf.ret_addr[f_trace_buf.f_trace_end] = ret_addr;
+            f_trace_buf.function[f_trace_buf.f_trace_end] = f_info[i];
+            f_trace_buf.ret_addr[f_trace_buf.f_trace_end] = ret_addr;
             f_trace_buf.is_ret[f_trace_buf.f_trace_end] = false;
             Log("jump to 0x%lx(%s)", f_trace_buf.function[f_trace_buf.f_trace_end].f_addr, f_trace_buf.function[f_trace_buf.f_trace_end].f_name);
 
