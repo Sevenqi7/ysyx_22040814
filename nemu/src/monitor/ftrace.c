c5e65afa5066bcf913eb1178f8a7ff1299e80314
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-16 00:10:15 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-67-generic #74-Ubuntu SMP Wed Feb 22 14:14:39 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:10:15 up  8:28,  5 users,  load average: 1.10, 0.96, 0.79

--- nemu/src/monitor/ftrace.c
+++ nemu/src/monitor/ftrace.c
@@ -42,7 +42,7 @@ void ftrace_check_jal(vaddr_t jump_addr, vaddr_t ret_addr, int rd)
         else if(f_info[i].f_addr == f_trace_buf.ret_addr[f_trace_buf.f_trace_end])
         {
             f_trace_buf.function[f_trace_buf.f_trace_end] = f_info[i];
-            f_trace_buf.is_ret[f_trace_buf.f_trace_end] = false;
+            f_trace_buf.is_ret[f_trace_buf.f_trace_end] = true;
             Log("%lx ret", f_trace_buf.function[f_trace_buf.f_trace_end].f_addr);
             f_trace_buf.f_trace_end++;
             return ;
