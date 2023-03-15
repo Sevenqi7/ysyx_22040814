826b9cb974018525c20c33a70b4e2e03c5596c3a
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-15 22:02:23 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-67-generic #74-Ubuntu SMP Wed Feb 22 14:14:39 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:02:23 up  6:20,  5 users,  load average: 1.30, 1.02, 0.90

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -239,27 +239,32 @@ void sdb_get_symbol_list(char *elf_path)
       printf("Failed to read Elf Header\n");
       return ;
   }
-
+  //read .shstrtab
+  lseek(fd, ehdr.e_shoff + ehdr.e_shentsize * ehdr.e_shstrndx, SEEK_SET);
   Elf64_Shdr shdr;
+  if(read(fd, &shdr, sizeof(shdr)) != sizeof(Elf64_Shdr))
+  {
+      printf("Failed to read section header of string table!\n");
+      return ;
+  }
+  char *shstrtab = malloc(shdr.sh_size);
+  if(read(fd, shstrtab, shdr.sh_size) != shdr.sh_size)
+  {
+      printf("Failed to read section .shstrtab!\n");
+      return ;
+  }
+
+  //read shstrtab
   for(int i=0;i<ehdr.e_shnum;i++)
   {
-      char section_name[20];
       lseek(fd, ehdr.e_shoff + ehdr.e_shentsize * i, SEEK_SET);
       if(read(fd, &shdr, sizeof(Elf64_Shdr)) != sizeof(Elf64_Shdr))
-      {
-          printf("Failed to read Elf Section Header!\n");
-          return ;
-      }
-      lseek(fd, shdr.sh_name, SEEK_SET);
-      if(read(fd, section_name, 19) == 0)
           return;
-      if(shdr.sh_type == SHT_STRTAB && !strcmp(".strtab", section_name))
-      {
-      assert(0);
-          printf("strtab found\n");
-          break;
-      }
+      if(shdr.sh_type == SHT_STRTAB && !strcmp(".strtab", shstrtab + shdr.sh_name)){
+          assert(0);
+          break;}
   }
+
   char *strtab = malloc(shdr.sh_size);
   lseek(fd, shdr.sh_offset, SEEK_SET);
   if(read(fd, strtab, shdr.sh_size) != shdr.sh_size)
