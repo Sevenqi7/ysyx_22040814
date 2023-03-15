f2da212e62aa7de1d89b3e6fb8d69fad12bafefb
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-15 17:55:25 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-67-generic #74-Ubuntu SMP Wed Feb 22 14:14:39 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:55:25 up  2:13,  4 users,  load average: 0.83, 0.75, 0.83

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -57,14 +57,14 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
-  #ifdef CONFIG_MTRACE
-      printf("      Read %d bytes from %x\n", len, addr);
-  #endif
-  // word_t read_data = 0;
+  word_t read_data = 0;
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
-  IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
+  IFDEF(CONFIG_DEVICE, read_data = mmio_read(addr, len));
   out_of_bound(addr);
-  return 0;
+  #ifdef CONFIG_MTRACE
+      printf("      Read data %lx with %d bytes from %x\n", read_data, len, addr);
+  #endif
+  return read_data;
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
