e92866a93af6e3fafbcd1b0022183c8b8980879c
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-16 22:22:58 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-67-generic #74-Ubuntu SMP Wed Feb 22 14:14:39 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:22:58 up 1 day,  6:41,  7 users,  load average: 1.81, 1.46, 1.50

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -8,157 +8,71 @@
 static void itoa(int x, char str[]);
 
 
-int printf(const char *format, ...) {
+
+int printf(const char *fmt, ...) {
   va_list args;
-  va_start(args, format);
-  int count = 0;
-  while (*format != '\0') {
-    if (*format == '%') {
-      format++;
-      if (*format == '%') {
-        putch('%');
-        count++;
-      } else if (*format == 'c') {
-        char c = va_arg(args, int);
-        putch(c);
-        count++;
-      } else if (*format == 'd' || *format == 'i') {
-        int num = va_arg(args, int);
-        if (num < 0) {
-          putch('-');
-          count++;
-          num = -num;
-        }
-        int digits = 0;
-        int tmp = num;
-        while (tmp) {
-          tmp /= 10;
-          digits++;
-        }
-        while (digits < (*++format - '0')) {
-          putch('0');
-          count++;
-        }
-        if (num == 0) {
-          putch('0');
-          count++;
-        } else {
-          while (num) {
-            int digit = num % 10;
-            putch(digit + '0');
-            count++;
-            num /= 10;
+  va_start(args, fmt);
+  int ret = 0;
+  for(int i=0;fmt[i];i++)
+  {
+      if(fmt[i] == '%')
+      {
+          i++;
+          switch(fmt[i])
+          {
+                case 'c':
+                  char ch = va_arg(args, int);
+                  putch(ch), ret++; 
+                  break;
+                case 's': 
+                  for(char *s = va_arg(args, char *);*s;s++) putch(*s), ret++;
+                  break;
+                case 'd':
+                  char num[32];       //max of int is 2147483647
+                  int x = va_arg(args, int);
+                  itoa(x, num);
+                  for(int j=0;num[j];j++) putch(num[j]);
+                  break;
+                case 'u':
+                    unsigned int n = va_arg(args, uint32_t);
+                    do{
+                        char ch = n % 10 + '0';
+                        putch(ch);
+                    } while(n /= 10);
+                  break;
+                case '0':
+                    i++;
+                    char num2[20];
+                    memset(num2, 10, 20);
+                    for(int j=0;fmt[i] < '9' && fmt[i] >'0';i++)
+                        num2[j++] = fmt[i] - '0';
+                    if(fmt[i] == 'd')
+                    { 
+                        int zero_num = 0;
+                        for(int j=0;num2[j] != 10;j++) 
+                            zero_num = zero_num * 10 + num2[j];
+                        int x = va_arg(args, int);
+                        itoa(x, num2);
+                        int j=0;
+                        for(j=0;x;j++)  x /= 10;
+                        while(--zero_num > j) putch('0');
+                        for(j=0;num2[j];j++) putch(num2[j]);
+                    }
+                    break;
+                default:
+                    putch(fmt[i]);
+                    while(1);
+                    panic("Unimplemented argument!");
           }
-        }
-      } else if (*format == 's') {
-        char *str = va_arg(args, char *);
-        while (*str) {
-          putch(*str++);
-          count++;
-        }
-      } else if (*format == 'x' || *format == 'X') {
-        unsigned int num = va_arg(args, unsigned int);
-        char hex_digits[] = "0123456789ABCDEF";
-        int shift = (*format == 'x' ? 0 : 4);
-        int mask = 0xF << shift;
-        int digits = 0;
-        unsigned int tmp = num;
-        while (tmp) {
-          tmp >>= 4;
-          digits++;
-        }
-        while (digits < (*++format - '0')) {
-          putch('0');
-          count++;
-        }
-        if (num == 0) {
-          putch('0');
-          count++;
-        } else {
-          while (num) {
-            int digit = (num & mask) >> shift;
-            putch(hex_digits[digit]);
-            count++;
-            num >>= 4;
-          }
-        }
       }
-    } else {
-      putch(*format);
-      count++;
-    }
-    format++;
-  }
+      else
+      {
+            putch(fmt[i]);
+      }
+  } 
   va_end(args);
-  return count;
+  return ret;
 }
-
-
-
-// int printf(const char *fmt, ...) {
-//   va_list args;
-//   va_start(args, fmt);
-//   int ret = 0;
-//   for(int i=0;fmt[i];i++)
-//   {
-//       if(fmt[i] == '%')
-//       {
-//           i++;
-//           switch(fmt[i])
-//           {
-//                 case 'c':
-//                   char ch = va_arg(args, int);
-//                   putch(ch), ret++; 
-//                   break;
-//                 case 's': 
-//                   for(char *s = va_arg(args, char *);*s;s++) putch(*s), ret++;
-//                   break;
-//                 case 'd':
-//                   char num[32];       //max of int is 2147483647
-//                   int x = va_arg(args, int);
-//                   itoa(x, num);
-//                   for(int j=0;num[j];j++) putch(num[j]);
-//                   break;
-//                 case 'u':
-//                     unsigned int n = va_arg(args, uint32_t);
-//                     do{
-//                         char ch = n % 10 + '0';
-//                         putch(ch);
-//                     } while(n /= 10);
-//                   break;
-//                 case '0':
-//                     i++;
-//                     char num2[20];
-//                     memset(num2, 10, 20);
-//                     for(int j=0;fmt[i] < '9' && fmt[i] >'0';i++)
-//                         num2[j++] = fmt[i] - '0';
-//                     if(fmt[i] == 'd')
-//                     { 
-//                         int zero_num = 0;
-//                         for(int j=0;num2[j] != 10;j++) 
-//                             zero_num = zero_num * 10 + num2[j];
-//                         int x = va_arg(args, int);
-//                         itoa(x, num2);
-//                         int j=0;
-//                         for(j=0;x;j++)  x /= 10;
-//                         while(--zero_num > j) putch('0');
-//                         for(j=0;num2[j];j++) putch(num2[j]);
-//                     }
-//                     break;
-//                 default:
-//                     putch(fmt[i]);
-//                     while(1);
-//                     panic("Unimplemented argument!");
-//           }
-//       }
-//       else
-//       {
-//             putch(fmt[i]);
-//       }
-//   } 
-//   va_end(args);
-//   return ret;
-// }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
   int len = 0;
