07d9f766214a136b123208b1db152115a34a215b
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-16 22:35:24 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-67-generic #74-Ubuntu SMP Wed Feb 22 14:14:39 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:35:24 up 1 day,  6:53,  7 users,  load average: 1.06, 0.97, 1.19

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -7,15 +7,15 @@
 static int width, height;
 
 void __am_gpu_init() {
-  // int i;
+  int i;
   uint32_t screen_size = inl(VGACTL_ADDR);
   width = screen_size >> 16;
   height = screen_size & 0xffff;
   int w = screen_size >> 16;  // TODO: get the correct width
   int h = screen_size & 0xffff;  // TODO: get the correct height
   printf("\nw:%d, h:%d\n", w, h);
-  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  // for (i = 0; i < w * h; i ++) fb[i] = i;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i ++) fb[i] = i;
   outl(SYNC_ADDR, 1);
 }
 
@@ -33,7 +33,7 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
 
-  uint32_t *draw_addr = (uint32_t *)(uintptr_t)(FB_ADDR + ctl->y * 400);
+  uint32_t *draw_addr = (uint32_t *)(uintptr_t)(FB_ADDR + ctl->y * 400 + ctl->x);
   uint32_t *pixel = (uint32_t *)ctl->pixels;
   if(ctl->x)
   printf("\nx:%d, y:%d\n", ctl->x, ctl->y);
@@ -41,12 +41,13 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   for(int i=ctl->y;i<ctl->y+ctl->h;i++)
   {
       if(i >= height) break;
-      draw_addr = (uint32_t *)(uintptr_t)(FB_ADDR + ctl->x);
-      for(int k=ctl->x;k<ctl->x+ctl->w;k++)
+      int k;
+      for(k=ctl->x;k<ctl->x+ctl->w;k++)
       {
           if(k >= width) break;
           *draw_addr++ = *pixel++;
       }
+      draw_addr = (uint32_t *)(uintptr_t)(FB_ADDR + i * 400 + k);
   }
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
