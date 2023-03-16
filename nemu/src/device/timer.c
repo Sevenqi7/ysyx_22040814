5524d78b206b7beb6ffc278486747838f5f4fb67
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-16 15:34:45 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-67-generic #74-Ubuntu SMP Wed Feb 22 14:14:39 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:34:45 up 23:52,  6 users,  load average: 0.91, 1.09, 0.97

--- nemu/src/device/timer.c
+++ nemu/src/device/timer.c
@@ -20,11 +20,12 @@
 static uint32_t *rtc_port_base = NULL;
 
 static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
-  Log("offset:%d, len:%d, is_write:%d", offset, len ,is_write);
-  assert(0);
+  // Log("offset:%d, len:%d, is_write:%d", offset, len ,is_write);
   assert(offset == 0 || offset == 4);
   if (!is_write && offset == 4) {
     uint64_t us = get_time();
+    Log("us:%lu", us);
+    assert(0);
     rtc_port_base[0] = (uint32_t)us;
     rtc_port_base[1] = us >> 32;
   }
