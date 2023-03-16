9ab65981210ce4bc557ede4d1308c176266af653
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-16 18:07:52 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-67-generic #74-Ubuntu SMP Wed Feb 22 14:14:39 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:07:52 up 1 day,  2:26,  6 users,  load average: 0.53, 0.65, 0.72

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -41,6 +41,8 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
   int rs2 = BITS(i, 24, 20);
   *rd     = BITS(i, 11, 7);
   immB();
+  if(*rd == 9)
+    Log("s1 value:%lx", cpu.gpr[9]);
   // Log("immB:0x%lx rs1:%d. rs2:%d rd:%d", *imm, rs1, rs2, *rd);
   switch (type) {
     case TYPE_I: src1R();          immI(); break;
@@ -60,8 +62,6 @@ static int decode_exec(Decode *s) {
   // int rs1 = BITS(s->isa.inst.val, 19, 15);
   word_t src1 = 0, src2 = 0, imm = 0, shamt = BITS(s->isa.inst.val, 25, 20);
   s->dnpc = s->snpc;
-  if(rd == 9)
-  Log("s1 value:%lx", cpu.gpr[9]);
 
 #define INSTPAT_INST(s) ((s)->isa.inst.val)
 #define INSTPAT_MATCH(s, name, type, ... /* execute body */ ) { \
