bd8951bd25a90a186b9a61eea7975e7acc6f7108
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-16 18:05:24 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-67-generic #74-Ubuntu SMP Wed Feb 22 14:14:39 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:05:24 up 1 day,  2:23,  6 users,  load average: 0.97, 0.71, 0.74

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -60,6 +60,8 @@ static int decode_exec(Decode *s) {
   // int rs1 = BITS(s->isa.inst.val, 19, 15);
   word_t src1 = 0, src2 = 0, imm = 0, shamt = BITS(s->isa.inst.val, 25, 20);
   s->dnpc = s->snpc;
+  if(rd == 0)
+  Log("s1 value:%lx", cpu.gpr[9]);
 
 #define INSTPAT_INST(s) ((s)->isa.inst.val)
 #define INSTPAT_MATCH(s, name, type, ... /* execute body */ ) { \
