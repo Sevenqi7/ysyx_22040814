2da08ebda07e1f77c904638338530eefd6531b47
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-16 16:18:08 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-67-generic #74-Ubuntu SMP Wed Feb 22 14:14:39 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:18:08 up 1 day, 36 min,  6 users,  load average: 1.38, 1.13, 1.01

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -48,6 +48,7 @@ int printf(const char *fmt, ...) {
                     { 
                         int zero_num = 0;
                         for(int j=0;num2[j] != -1;j++) zero_num = zero_num * 10 + num2[j];
+                        assert(0);
                         int x = va_arg(args, int);
                         itoa(x, num2);
                         int j=0;
