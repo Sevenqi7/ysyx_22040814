5a61902381e082fddae4cac221a203e31d130d8e
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-16 15:45:01 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-67-generic #74-Ubuntu SMP Wed Feb 22 14:14:39 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:45:01 up 1 day, 3 min,  6 users,  load average: 1.16, 1.00, 0.97

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,5 +1,6 @@
 #include <am.h>
 #include <nemu.h>
+#include <stdio.h>
 
 // static uint64_t __am_timer_boot_time = 0;
 
@@ -10,7 +11,7 @@ void __am_timer_init() {
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   // uptime->us = inl(RTC_ADDR);
   uptime->us = inl(RTC_ADDR+4);
-
+  printf("us:%d", uptime->us);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
