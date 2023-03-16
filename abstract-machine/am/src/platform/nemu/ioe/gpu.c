b8dd752ed6d0e525dcff39032a319711a595ec96
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-16 20:56:33 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-67-generic #74-Ubuntu SMP Wed Feb 22 14:14:39 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:56:33 up 1 day,  5:14,  7 users,  load average: 0.80, 0.99, 1.00

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,14 +4,25 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+  int i;
+  uint32_t screen_size = inl(VGACTL_ADDR);
+
+  int w = screen_size << 16;  // TODO: get the correct width
+  int h = screen_size & 0xf0;  // TODO: get the correct height
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i ++) fb[i] = i;
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  *cfg = (AM_GPU_CONFIG_T) {
-    .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
-  };
+  // *cfg = (AM_GPU_CONFIG_T) {
+  //   .present = true, .has_accel = false,
+  //   .width = 0, .height = 0,
+  //   .vmemsz = 0
+  // };
+      uint32_t screen_size = inl(VGACTL_ADDR);
+      cfg->width = screen_size >> 16;
+      cfg->height = screen_size & 0x0f;
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
