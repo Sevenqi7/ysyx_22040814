addb2cb7ea510323ce4eaee7aa2ee7cf8a9726b2
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-16 15:35:53 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-67-generic #74-Ubuntu SMP Wed Feb 22 14:14:39 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:35:53 up 23:54,  6 users,  load average: 1.11, 1.09, 0.98

--- nemu/src/utils/timer.c
+++ nemu/src/utils/timer.c
@@ -40,7 +40,7 @@ static uint64_t get_time_internal() {
 
 uint64_t get_time() {
   if (boot_time == 0) boot_time = get_time_internal();
-  assert(0);
   uint64_t now = get_time_internal();
+  assert(0);
   return now - boot_time;
 }
