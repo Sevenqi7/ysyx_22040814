4114f10bee9230af4ce00206d98185e4f9afc502
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-16 15:41:47 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-67-generic #74-Ubuntu SMP Wed Feb 22 14:14:39 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:41:47 up 1 day, 0 min,  6 users,  load average: 0.71, 0.90, 0.94

--- nemu/src/device/timer.c
+++ nemu/src/device/timer.c
@@ -29,7 +29,6 @@ static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
     rtc_port_base[0] = (uint32_t)us;
     rtc_port_base[1] = us >> 32;
   }
-  assert(0);
 }
 
 #ifndef CONFIG_TARGET_AM
