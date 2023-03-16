2bfa11f8bd287c6ca0a8cd6cf7d5dfee901f8052
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-16 20:29:03 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-67-generic #74-Ubuntu SMP Wed Feb 22 14:14:39 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:29:03 up 1 day,  4:47,  7 users,  load average: 1.03, 1.00, 1.05

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -7,7 +7,7 @@
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   int scan_code = inl(KBD_ADDR);
   kbd->keydown = scan_code & KEYDOWN_MASK;
-  if(kbd->keydown)
+  if(!kbd->keydown)
       kbd->keycode = scan_code;
   else
       kbd->keycode = AM_KEY_NONE;
