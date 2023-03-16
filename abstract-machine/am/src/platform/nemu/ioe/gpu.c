a31088363e22e0bcd63586ae1d6699597192fcaf
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-16 22:54:23 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-67-generic #74-Ubuntu SMP Wed Feb 22 14:14:39 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:54:23 up 1 day,  7:12,  7 users,  load average: 1.84, 1.45, 1.32

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -37,7 +37,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   uint32_t *pixel = (uint32_t *)ctl->pixels;
   if(ctl->x)
   printf("\nx:%d, y:%d\n", ctl->x, ctl->y);
-
+  while(1);
   for(int i=ctl->y;i<ctl->y+ctl->h;i++)
   {
       if(i >= height) break;
