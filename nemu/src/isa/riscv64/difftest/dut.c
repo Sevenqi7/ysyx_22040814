8bd626a2b0c700f505cb45c1d745abb5b6beab22
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-16 17:58:17 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-67-generic #74-Ubuntu SMP Wed Feb 22 14:14:39 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:58:17 up 1 day,  2:16,  6 users,  load average: 0.91, 0.71, 0.78

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -17,12 +17,14 @@
 #include <cpu/difftest.h>
 #include "../local-include/reg.h"
 
+extern const char *regs[];
+
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   for(int i=0;i<32;i++)
   {
       if(ref_r->gpr[i] != cpu.gpr[i])
       {
-          Log("Difftest found gpr %d value is %lx, should be %lx", i, cpu.gpr[i], ref_r->gpr[i]);
+          Log("Difftest found %s value is %lx, should be %lx", regs[i], cpu.gpr[i], ref_r->gpr[i]);
           return false;
       }
   }
