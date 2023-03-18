ea71d4877909c52a138427066672699a0de10350
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-18 21:21:08 +0800

    > sim RTL ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-67-generic #74-Ubuntu SMP Wed Feb 22 14:14:39 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:21:08 up 27 min,  3 users,  load average: 0.57, 0.97, 0.90

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -7,4 +7,13 @@ module top(
 
 assign out = in1 | in2;
 
+   initial begin
+      if ($test$plusargs("trace") != 0) begin
+         $display("[%0t] Tracing to logs/vlt_dump.vcd...\n", $time);
+         $dumpfile("logs/vlt_dump.vcd");
+         $dumpvars();
+      end
+      $display("[%0t] Model running...\n", $time);
+   end
+
 endmodule
