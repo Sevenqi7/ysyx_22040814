59a83764dd9c6e9eae089495d90bdc0c65c40134
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-18 21:25:28 +0800

    > sim RTL ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-67-generic #74-Ubuntu SMP Wed Feb 22 14:14:39 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:25:28 up 31 min,  3 users,  load average: 1.93, 1.47, 1.12

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,12 +1,86 @@
-#include <Vtop.h>
+// DESCRIPTION: Verilator: Verilog example module
+//
+// This file ONLY is placed under the Creative Commons Public Domain, for
+// any use, without warranty, 2017 by Wilson Snyder.
+// SPDX-License-Identifier: CC0-1.0
+//======================================================================
 
-static Vtop dut;
+// For std::unique_ptr
+#include <memory>
 
-int main() {
-  dut.in1 = 1;
-  dut.in2 = 0;
-  while(1) {
-    dut.eval();
-  }
+// Include common routines
+#include <verilated.h>
+
+// Include model header, generated from Verilating "top.v"
+#include "Vtop.h"
+
+int main(int argc, char** argv, char** env) {
+    // This is a more complicated example, please also see the simpler examples/make_hello_c.
+
+    // Prevent unused variable warnings
+    if (false && argc && argv && env) {}
+
+    // Create logs/ directory in case we have traces to put under it
+    Verilated::mkdir("logs");
+
+    // Construct a VerilatedContext to hold simulation time, etc.
+    // Multiple modules (made later below with Vtop) may share the same
+    // context to share time, or modules may have different contexts if
+    // they should be independent from each other.
+
+    // Using unique_ptr is similar to
+    // "VerilatedContext* contextp = new VerilatedContext" then deleting at end.
+    const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
+
+    // Set debug level, 0 is off, 9 is highest presently used
+    // May be overridden by commandArgs argument parsing
+    contextp->debug(0);
+
+    // Randomization reset policy
+    // May be overridden by commandArgs argument parsing
+    contextp->randReset(2);
+
+    // Verilator must compute traced signals
+    contextp->traceEverOn(true);
+
+    // Pass arguments so Verilated code can see them, e.g. $value$plusargs
+    // This needs to be called before you create any model
+    contextp->commandArgs(argc, argv);
+
+    // Construct the Verilated model, from Vtop.h generated from Verilating "top.v".
+    // Using unique_ptr is similar to "Vtop* top = new Vtop" then deleting at end.
+    // "TOP" will be the hierarchical name of the module.
+    const std::unique_ptr<Vtop> top{new Vtop{contextp.get(), "TOP"}};
+
+    // Set Vtop's input signals
+    top->in1 = 0;
+    top->in2 = 0;
+
+    // Simulate until $finish
+    while (!contextp->gotFinish()) {
+        contextp->timeInc(1);  // 1 timeprecision period passes...
+        if (!top->clk) {
+            if (contextp->time() > 1 && contextp->time() < 10) {
+                top->in1 = !1;  // Assert reset
+            } else {
+                top->in2 = !0;  // Deassert reset
+            }
+        }
+
+        top->eval();
+
+    }
+
+    // Final model cleanup
+    top->final();
+
+    // Coverage analysis (calling write only after the test is known to pass)
+#if VM_COVERAGE
+    Verilated::mkdir("logs");
+    contextp->coveragep()->write("logs/coverage.dat");
+#endif
+
+    // Return good completion status
+    // Don't use exit() or destructor won't get called
+    return 0;
 }
-
