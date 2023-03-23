83ec334c3950be7d2c553f65654c4f85ec7a575e
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-24 03:07:17 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-67-generic #74-Ubuntu SMP Wed Feb 22 14:14:39 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  03:07:17 up 1 day,  3:40,  3 users,  load average: 0.93, 0.90, 0.99

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -18,37 +18,37 @@
 #include <difftest-def.h>
 #include <memory/paddr.h>
 
-void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
-  if(addr < PMEM_LEFT || addr > PMEM_RIGHT)
-  {
-      printf("Difftest addr out of bound\n");
-      assert(0);   
-  }
-  char *p = buf;
-  if(direction == DIFFTEST_TO_DUT)
-    while(n--)
-      *p++ = paddr_read(addr++, 1);
-  else if(direction == DIFFTEST_TO_REF)
-    while(n--)
-      paddr_write(addr++, 1, *p++);
-}
+// void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
+//   if(addr < PMEM_LEFT || addr > PMEM_RIGHT)
+//   {
+//       printf("Difftest addr out of bound\n");
+//       assert(0);   
+//   }
+//   char *p = buf;
+//   if(direction == DIFFTEST_TO_DUT)
+//     while(n--)
+//       *p++ = paddr_read(addr++, 1);
+//   else if(direction == DIFFTEST_TO_REF)
+//     while(n--)
+//       paddr_write(addr++, 1, *p++);
+// }
 
-void difftest_regcpy(void *dut, bool direction) {
-  if(direction == DIFFTEST_TO_DUT)
-      memcpy(dut, &cpu, sizeof(cpu));
-  else if(direction == DIFFTEST_TO_DUT)
-      memcpy(&cpu, dut, sizeof(cpu));
-}
+// void difftest_regcpy(void *dut, bool direction) {
+//   if(direction == DIFFTEST_TO_DUT)
+//       memcpy(dut, &cpu, sizeof(cpu));
+//   else if(direction == DIFFTEST_TO_DUT)
+//       memcpy(&cpu, dut, sizeof(cpu));
+// }
 
-void difftest_exec(uint64_t n) {
-  cpu_exec(n);
-}
+// void difftest_exec(uint64_t n) {
+//   cpu_exec(n);
+// }
 
-void difftest_raise_intr(word_t NO) {
-  assert(0);
-}
+// void difftest_raise_intr(word_t NO) {
+//   assert(0);
+// }
 
-void difftest_init(int port) {
-  /* Perform ISA dependent initialization. */
-  init_isa();
-}
+// void difftest_init(int port) {
+//   /* Perform ISA dependent initialization. */
+//   init_isa();
+// }
