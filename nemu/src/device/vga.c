4572d6653bb8606ddbd87677098a474b64a2a752
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-26 18:07:18 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-67-generic #74-Ubuntu SMP Wed Feb 22 14:14:39 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:07:18 up  4:18,  6 users,  load average: 2.09, 2.37, 2.18

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -56,7 +56,6 @@ static void init_screen() {
 }
 
 static inline void update_screen() {
-  Log("SCREENW * size:%lu", SCREEN_W * sizeof(uint32_t));
   SDL_UpdateTexture(texture, NULL, vmem, SCREEN_W * sizeof(uint32_t));
   SDL_RenderClear(renderer);
   SDL_RenderCopy(renderer, texture, NULL, NULL);
