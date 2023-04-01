94122d08d6168e42d88c2047e1b83eb189bf7290
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-01 21:24:27 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-69-generic #76-Ubuntu SMP Fri Mar 17 17:19:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:24:27 up  2:03,  5 users,  load average: 0.67, 0.70, 0.55

--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -43,8 +43,9 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
-
-  return 0;
+  cpu.csr.mepc = epc;
+  cpu.csr.mcause = NO;
+  return cpu.csr.mtvec;
 }
 
 word_t isa_query_intr() {
