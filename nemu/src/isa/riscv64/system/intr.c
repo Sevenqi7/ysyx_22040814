ffaeb39df90343ef119fe44f1bb258b25ded70f9
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-01 21:08:52 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-69-generic #76-Ubuntu SMP Fri Mar 17 17:19:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:08:52 up  1:47,  5 users,  load average: 0.64, 0.50, 0.39

--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -20,6 +20,7 @@ word_t isa_read_csr(word_t NO)
   switch(NO)
   {
     case 0x300: return cpu.csr.mstatus; break;
+    case 0x305: return cpu.csr.mtvec  ; break;
     case 0x341: return cpu.csr.mepc   ; break;
     case 0x342: return cpu.csr.mcause ; break;
     default: assert(0);
@@ -31,6 +32,7 @@ void isa_write_csr(word_t NO, word_t data)
   switch(NO)
   {
     case 0x300: cpu.csr.mstatus = data; break;
+    case 0x305: cpu.csr.mtvec   = data; break;
     case 0x341: cpu.csr.mepc    = data; break;
     case 0x342: cpu.csr.mcause  = data; break;
     default: assert(0);
