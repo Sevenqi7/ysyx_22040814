53e4c853a570a786b0429208ef94796d84083e73
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-02 17:12:55 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-69-generic #76-Ubuntu SMP Fri Mar 17 17:19:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:12:55 up  3:49,  6 users,  load average: 1.80, 1.39, 1.01

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -3,151 +3,104 @@
 #include <klib-macros.h>
 #include <stdarg.h>
 
+#define PRINTFBUF_SIZE 4096
+
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-static void itoa(int x, char str[]);
+//移植了XV6里的printf
+struct sprintbuf {
+	char *buf;
+	char *ebuf;
+	int cnt;
+};
 
+struct printfbuf {
+	char buf[PRINTFBUF_SIZE];
+	char *buf_p;
+	int cnt;
+};
+
+static struct printfbuf printfb = {
+	.buf_p = printfb.buf
+};
+
+void vprintfmt(void (*putch)(int, void*), void *putdat, const char *fmt, va_list ap);
+
+static void
+printfputch(int ch, struct printfbuf *b)
+{
+	b->cnt++;
+	*b->buf_p++ = (char)ch;
+	if (ch == '\n' || b->buf_p == b->buf + PRINTFBUF_SIZE) {
+        for(int i=0;i<b->buf_p - b->buf;i++)
+            putch(b->buf[i]);
+		// write(STDOUT, b->buf, b->buf_p - b->buf);
+		b->buf_p = b->buf;
+	}
+}
+
+static void
+sprintputch(int ch, struct sprintbuf *b)
+{
+	b->cnt++;
+	if (b->buf < b->ebuf)
+		*b->buf++ = ch;
+}
+
+int
+vprintf(const char *fmt, va_list ap)
+{
+	struct printfbuf *b = &printfb;
+	b->cnt = 0;
+	vprintfmt((void *)printfputch, b, fmt, ap);	
+	int rc = b->cnt;
+	return rc;
+}
 
 
 int printf(const char *fmt, ...) {
-  va_list args;
-  va_start(args, fmt);
-  int ret = 0;
-  for(int i=0;fmt[i];i++)
-  {
-      if(fmt[i] == '%')
-      {
-          i++;
-          switch(fmt[i])
-          {
-                case 'c':
-                  char ch = va_arg(args, int);
-                  putch(ch), ret++; 
-                  break;
-                case 's': 
-                  for(char *s = va_arg(args, char *);*s;s++) putch(*s), ret++;
-                  break;
-                case 'd':
-                  char num[32];       //max of int is 2147483647
-                  int x = va_arg(args, int);
-                  itoa(x, num);
-                  for(int j=0;num[j];j++) putch(num[j]);
-                  break;
-                case 'u':
-                    unsigned int n = va_arg(args, uint32_t);
-                    do{
-                        char ch = n % 10 + '0';
-                        putch(ch);
-                    } while(n /= 10);
-                  break;
-                case '0':
-                    i++;
-                    char num2[20];
-                    memset(num2, 10, 20);
-                    for(int j=0;fmt[i] < '9' && fmt[i] >'0';i++)
-                        num2[j++] = fmt[i] - '0';
-                    if(fmt[i] == 'd')
-                    { 
-                        int zero_num = 0;
-                        for(int j=0;num2[j] != 10;j++) 
-                            zero_num = zero_num * 10 + num2[j];
-                        int x = va_arg(args, int);
-                        itoa(x, num2);
-                        int j=0;
-                        for(j=0;x;j++)  x /= 10;
-                        while(--zero_num > j) putch('0');
-                        for(j=0;num2[j];j++) putch(num2[j]);
-                    }
-                    break;
-                default:
-                    putch(fmt[i]);
-                    putch('\n');
-                    panic("Unimplemented argument!");
-          }
-      }
-      else
-      {
-            putch(fmt[i]);
-      }
-  } 
-  va_end(args);
-  return ret;
+	va_list ap;
+	int rc;
+
+	va_start(ap, fmt);
+	rc = vprintf(fmt, ap);
+	va_end(ap);
+
+	return rc;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  int len = 0;
-  char *p = out;
-  for(int i=0;fmt[i];i++)
-  {
-      if(fmt[i] == '%')
-      {
-          i++;
-          switch(fmt[i])
-          {
-              case 's':
-                  char *str = va_arg(ap, char*);
-                  for(int j=0;str[j];j++) *p++ = str[j], len++;
-                  break;
-              case 'd':
-                  char num[32];       //max of int is 2147483647
-                  int x = va_arg(ap, int);
-                  itoa(x, num);
-                  for(int j=0;num[j];j++) *p++ = num[j], len++;
-                  break;
-              default:
-                  panic("Unimplemented Argument in vsprintf!\n");
-          }
-      }
-      else
-      {
-          *p++ = fmt[i];
-          len++;
-      }
-  }
-  *p = '\0';
-  
-  return len;
+    return 0;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  va_list args;
-  va_start(args, fmt);
-  int ret = vsprintf(out, fmt, args);
-  va_end(args);
-  return ret;
-}
-
-int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+    return 0;
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+	struct sprintbuf b = {out, out+n-1, 0};
+
+	if (out == NULL || n < 1)
+		return -1;
+
+	// print the string to the buffer
+	vprintfmt((void*)sprintputch, &b, fmt, ap);
+
+	// null terminate the buffer
+	*b.buf = '\0';
+
+	return b.cnt;
 }
 
-static void itoa(int n, char s[])
-{
-    int i, sign;
+int snprintf(char *out, size_t n, const char *fmt, ...) {
+	va_list ap;
+	int rc;
 
-    if ((sign = n) < 0)  
-        n = -n;          
+	va_start(ap, fmt);
+	rc = vsnprintf(out, n, fmt, ap);
+	va_end(ap);
 
-    i = 0;
-    do {
-        s[i++] = n % 10 + '0';  
-    } while ((n /= 10) > 0);   
-
-    if (sign < 0)
-        s[i++] = '-';
-
-    s[i] = '\0';
-
-    int j, temp;
-    for (j = 0, i--; j < i; j++, i--) {
-        temp = s[j];
-        s[j] = s[i];
-        s[i] = temp;
-    }
+	return rc;
 }
 
 #endif
