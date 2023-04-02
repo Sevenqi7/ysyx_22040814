8bfe7b75ba65f36f89ae6834459d766d51512731
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-02 20:05:31 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-69-generic #76-Ubuntu SMP Fri Mar 17 17:19:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:05:31 up  6:42,  4 users,  load average: 0.38, 0.96, 1.34

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -4,9 +4,9 @@
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-
+  Log("SYSCALL occured, id:%d", a[0]);
   switch (a[0]) {
-    case SYS_exit : halt(c->gpr[10]) ; 
+    case SYS_exit : halt(c->gpr[10]);
     case SYS_yield: yield(); c->gpr[10] = 0; break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
