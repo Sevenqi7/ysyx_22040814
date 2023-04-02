c0f1f78ccc098ae4b65523c660d460b9d63a90e1
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-02 20:20:40 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-69-generic #76-Ubuntu SMP Fri Mar 17 17:19:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:20:40 up  6:57,  5 users,  load average: 1.36, 1.15, 1.13

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -12,10 +12,14 @@ char *syscall_name[] =
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
   switch (a[0]) {
     case SYS_exit : halt(c->gpr[10]);
     case SYS_yield: yield(); c->gpr[10] = 0; break;
+    // case SYS_write: 
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
-  Log("STRACE: SYS_%s called, retval:%d", syscall_name[a[0]], c->gpr[10]);
+  Log("STRACE: SYS_%s called, retval:%d, args:%d %d %d", syscall_name[a[0]], c->GPR2, c->GPR3, c->GPR4);
 }
