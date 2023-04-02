43afc4816691610f460de2b4ee103b16b2f23cd2
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-02 18:06:12 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-69-generic #76-Ubuntu SMP Fri Mar 17 17:19:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:06:12 up  4:43,  7 users,  load average: 0.43, 0.54, 0.51

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,8 +9,20 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
+  // TODO();
+  Elf_Ehdr eh;
+  Elf_Phdr ph;
+  ramdisk_read(&eh, 0, sizeof(Elf_Ehdr));
+  for(int i=0;i<eh.e_phnum;i++)
+  {
+    ramdisk_read(&ph, eh.e_phoff + i * sizeof(Elf_Phdr), sizeof(Elf_Phdr));
+    ramdisk_read((uintptr_t *)ph.p_vaddr ,ph.p_offset, ph.p_memsz);
+    memset((uintptr_t *)(ph.p_vaddr + ph.p_filesz), 0, ph.p_memsz - ph.p_filesz);
+  }
   return 0;
 }
 
