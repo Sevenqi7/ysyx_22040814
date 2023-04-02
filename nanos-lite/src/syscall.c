4283c6daa70de46220eceb81dfc67aede4d88ec5
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-02 20:09:42 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-69-generic #76-Ubuntu SMP Fri Mar 17 17:19:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:09:42 up  6:46,  4 users,  load average: 1.28, 1.04, 1.28

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,13 +1,21 @@
 #include <common.h>
 #include "syscall.h"
 
+char *syscall_name[] =
+{
+  "exit", "yield", "open", "read", "write", "kill",
+  "getpid", "close", "lseek", "brk", "fstat", "time",
+  "signal", "execve", "fork", "link", "unlink", "wait",
+  "times", "gettimeofday"
+};
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-  Log("SYSCALL occured, id:%d", a[0]);
   switch (a[0]) {
     case SYS_exit : halt(c->gpr[10]);
     case SYS_yield: yield(); c->gpr[10] = 0; break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
+  Log("STRACE: SYS_%s called, retval:%d", syscall_name[a[0]], c->gpr[10]);
 }
