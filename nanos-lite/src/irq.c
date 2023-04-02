12e5d9d657544644fb4aaa8b9340f99772c2999e
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-02 14:33:38 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-69-generic #76-Ubuntu SMP Fri Mar 17 17:19:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:33:38 up  1:10,  4 users,  load average: 0.97, 0.69, 0.64

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,7 +3,7 @@
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    case EVENT_YIELD: printf("Yield!\n"); while(1);
+    case EVENT_YIELD: printf("Yield!\n"); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
