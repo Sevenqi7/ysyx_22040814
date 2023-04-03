850e0f63cb9bd5225158ff06fd23b8a4e71f03ec
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-03 22:02:07 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-69-generic #76-Ubuntu SMP Fri Mar 17 17:19:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:02:06 up  3:26,  6 users,  load average: 0.67, 0.55, 0.58

--- nemu/src/monitor/ftrace.c
+++ nemu/src/monitor/ftrace.c
@@ -165,7 +165,7 @@ void sdb_get_symbol_list(char *elf_path)
                   // assert(0);
                   char *func_name = strtab + sym.st_name;
                   memcpy(f_info[f_info_num].f_name, func_name, strlen(func_name)+1);
-                  Log("symbol %d: %s", j, func_name);
+                //   Log("symbol %d: %s", j, func_name);
                   f_info[f_info_num].f_addr = sym.st_value;
                   f_info_num++;
               }
