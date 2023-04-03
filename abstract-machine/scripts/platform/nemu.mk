81daaf7c3483cac17047fd067195125aae590d16
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-03 21:42:17 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-69-generic #76-Ubuntu SMP Fri Mar 17 17:19:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:42:17 up  3:06,  5 users,  load average: 0.69, 0.71, 0.64

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -1,26 +1,25 @@
 AM_SRCS := platform/nemu/trm.c \
-           platform/nemu/ioe/ioe.c \
-           platform/nemu/ioe/timer.c \
-           platform/nemu/ioe/input.c \
-           platform/nemu/ioe/gpu.c \
-           platform/nemu/ioe/audio.c \
-           platform/nemu/ioe/disk.c \
-           platform/nemu/mpe.c
+		   platform/nemu/ioe/ioe.c \
+		   platform/nemu/ioe/timer.c \
+		   platform/nemu/ioe/input.c \
+		   platform/nemu/ioe/gpu.c \
+		   platform/nemu/ioe/audio.c \
+		   platform/nemu/ioe/disk.c \
+		   platform/nemu/mpe.c
 
 CFLAGS    += -fdata-sections -ffunction-sections
 LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
-             --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
-RAMDISK   ?=
+			 --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
 NEMUFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt
-NEMUFLAGS += -e $(IMAGE).elf -e $(RAMDISK)
+NEMUFLAGS += -e $(IMAGE).elf -e $(RAMDISK_FILE)
 
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 CFLAGS += -I$(AM_HOME)/am/src/platform/nemu/include
 .PHONY: $(AM_HOME)/am/src/platform/nemu/trm.c
 
 image: $(IMAGE).elf 
-    # @echo $(RAMDISK)
+	@echo $(RAMDISK_FILE)
 	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
 	@echo + OBJCOPY "->" $(IMAGE_REL).bin
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
