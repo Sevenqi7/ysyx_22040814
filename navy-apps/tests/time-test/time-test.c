1b201145fa73edde815dcf0246b59e56b97489be
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-04 03:02:06 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-69-generic #76-Ubuntu SMP Fri Mar 17 17:19:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  03:02:06 up  2:36,  7 users,  load average: 0.90, 0.76, 0.64

--- navy-apps/tests/time-test/time-test.c
+++ navy-apps/tests/time-test/time-test.c
@@ -9,13 +9,13 @@ int main()
     gettimeofday(&tv, NULL);
     while(1)
     {
-        last_ms = tv.tv_sec * 1000 + tv.tv_usec / 1000000;
+        last_ms = tv.tv_sec * 1000 + tv.tv_usec / 1000;
         while(gettimeofday(&tv, NULL))
         {
-            ms = tv.tv_sec * 1000 + tv.tv_usec / 1000000;
-            if(ms - last_ms >= 500) break;
+            ms = tv.tv_sec * 1000 + tv.tv_usec / 1000;
+            if(ms - last_ms >= 5000) break;
         }
-        printf("0.5s Pass.\n");
+        printf("5s Pass.\n");
         last_ms = ms;
     }
     return 0;
