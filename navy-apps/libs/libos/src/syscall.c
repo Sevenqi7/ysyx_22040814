f2a77eab26a8cd0fbdef548e8caecc6c079e56e8
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-03 12:07:54 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-69-generic #76-Ubuntu SMP Fri Mar 17 17:19:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  12:07:54 up  1:46,  5 users,  load average: 1.19, 0.92, 0.94

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -70,7 +70,8 @@ extern char _end;
 void *_sbrk(intptr_t increment) {
   static uintptr_t program_break = (uintptr_t)&_end;
   uintptr_t p_program_break = program_break;
-  int ret = _syscall_(SYS_brk, program_break + increment, 0, 0);
+  program_break += increment;
+  int ret = _syscall_(SYS_brk, program_break, 0, 0);
   if(!ret) return (void *)p_program_break;
   else return (void *)(-1);
 }
