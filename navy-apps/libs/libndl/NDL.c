6956885a999dcd1835ef9598f455bab53f2e165c
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-04 18:19:05 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-69-generic #76-Ubuntu SMP Fri Mar 17 17:19:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:19:05 up 17:53,  4 users,  load average: 1.19, 1.32, 1.41

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -92,6 +92,7 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   if(fd == -1) {printf("Failed to open /dev/fb!\n"); return;}
   uint64_t draw_offset = ((screen_h - h) / 2 * screen_w + (screen_w - w) / 2) * sizeof(uint32_t);
   Log("initial offset:%d", draw_offset);
+  while(1);
   for(int i=y;i<y+h;i++)
   {
     lseek(fd, draw_offset, SEEK_SET);
