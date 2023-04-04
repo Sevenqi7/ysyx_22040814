c0094cbe084dec315c9b22970f871f1708906200
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-04 16:25:19 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-69-generic #76-Ubuntu SMP Fri Mar 17 17:19:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:25:19 up 15:59, 10 users,  load average: 0.63, 1.00, 1.05

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -24,26 +24,8 @@ int NDL_PollEvent(char *buf, int len) {
   return 0;
 }
 
-void NDL_OpenCanvas(int *w, int *h) {
-  if (getenv("NWM_APP")) {
-    int fbctl = 4;
-    fbdev = 5;
-    screen_w = *w; screen_h = *h;
-    char buf[64];
-    int len = sprintf(buf, "%d %d", screen_w, screen_h);
-    // let NWM resize the window and create the frame buffer
-    write(fbctl, buf, len);
-    while (1) {
-      // 3 = evtdev
-      int nread = read(3, buf, sizeof(buf) - 1);
-      if (nread <= 0) continue;
-      buf[nread] = '\0';
-      if (strcmp(buf, "mmap ok") == 0) break;
-    }
-    close(fbctl);
-  }
-  else
-  {
+void NDL_GetCanvasSize(int *w, int *h)
+{
     char buf[64];
     int fd = open("/proc/dispinfo", 0, 0);
     if(fd == -1) {
@@ -75,8 +57,28 @@ void NDL_OpenCanvas(int *w, int *h) {
           *h = atoi(p);
         }
     }
+}
+
+void NDL_OpenCanvas(int *w, int *h) {
+  NDL_GetCanvasSize(w, h);
+  if (getenv("NWM_APP")) {
+    int fbctl = 4;
+    fbdev = 5;
+    screen_w = *w; screen_h = *h;
+    char buf[64];
+    int len = sprintf(buf, "%d %d", screen_w, screen_h);
+    // let NWM resize the window and create the frame buffer
+    write(fbctl, buf, len);
+    while (1) {
+      // 3 = evtdev
+      int nread = read(3, buf, sizeof(buf) - 1);
+      if (nread <= 0) continue;
+      buf[nread] = '\0';
+      if (strcmp(buf, "mmap ok") == 0) break;
+    }
+    close(fbctl);
   }
-  printf("width:%d height:%d\n", *w, *h);
+  printf("width:%d   height:%d\n", *w, *h);
 }
 
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
