f887ad75145714fddebf9b3b43a9f91cf144aace
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-04 21:45:09 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-69-generic #76-Ubuntu SMP Fri Mar 17 17:19:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:45:09 up 21:19,  5 users,  load average: 1.14, 0.93, 0.77

--- navy-apps/tests/float-test/float-test.c
+++ navy-apps/tests/float-test/float-test.c
@@ -7,9 +7,9 @@ int main()
 {
     char str_buf[255];
     fixedpt a = fixedpt_rconst(1.22121), b = fixedpt_rconst(-0.42121);
-    fixedpt_str(fixedpt_floor(a), str_buf, -1);
+    fixedpt_str(fixedpt_ceil(a), str_buf, -1);
     printf("%s\n", str_buf);
-    fixedpt_str(fixedpt_floor(b), str_buf, -1);
+    fixedpt_str(fixedpt_ceil(b), str_buf, -1);
     printf("%s\n", str_buf);
     return 0;
 }
