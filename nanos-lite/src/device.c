6899d2a3df15d6feb55057e51e980f021aee7d43
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-04 15:42:31 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-69-generic #76-Ubuntu SMP Fri Mar 17 17:19:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:42:31 up 15:16, 10 users,  load average: 1.49, 1.19, 0.84

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -29,7 +29,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   AM_GPU_CONFIG_T cfg;
   ioe_read(AM_GPU_CONFIG, &cfg);
-  return snprintf(buf, len, "WIDTH  :%d\nHEIGHT:  %d", cfg.width, cfg.height);
+  return snprintf(buf, len, "WIDTH  :%d\nHEIGHT:  %d\n", cfg.width, cfg.height);
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
