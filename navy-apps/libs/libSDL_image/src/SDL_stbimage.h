6d14edfac6f315c7a075ec536c7bd581e91199f4
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-05 21:13:56 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-69-generic #76-Ubuntu SMP Fri Mar 17 17:19:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:13:56 up 1 day, 20:48,  7 users,  load average: 1.61, 1.29, 1.08

--- navy-apps/libs/libSDL_image/src/SDL_stbimage.h
+++ navy-apps/libs/libSDL_image/src/SDL_stbimage.h
@@ -207,13 +207,13 @@ SDL_STBIMG_DEF SDL_Surface* STBIMG_LoadFromMemory(const unsigned char* buffer, i
 		SDL_SetError("STBIMG_LoadFromMemory(): Couldn't get image info: %s!\n", stbi_failure_reason());
 		return NULL;
 	}
-	printf("NULL\n");
 
 	// no alpha => use RGB, else use RGBA
 	origin_has_alpha = !(img.format == STBI_grey || img.format == STBI_rgb);
 	bppToUse = STBI_rgb_alpha;
 
 	img.data = stbi_load_from_memory(buffer, length, &img.w, &img.h, &img.format, bppToUse);
+	printf("NULL\n");
 	if(img.data == NULL)
 	{
 		SDL_SetError("STBIMG_LoadFromMemory(): Couldn't load image: %s!\n", stbi_failure_reason());
