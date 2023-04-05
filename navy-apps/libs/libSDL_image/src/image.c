14ca1d844a6172b119d8149d58be1bda7648a4ed
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-05 20:52:10 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-69-generic #76-Ubuntu SMP Fri Mar 17 17:19:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:52:10 up 1 day, 20:26,  5 users,  load average: 0.89, 0.77, 0.77

--- navy-apps/libs/libSDL_image/src/image.c
+++ navy-apps/libs/libSDL_image/src/image.c
@@ -1,5 +1,5 @@
-#define SDL_malloc  malloc
-#define SDL_free    free
+#define SDL_malloc malloc
+#define SDL_free free
 #define SDL_realloc realloc
 
 #define SDL_STBIMAGE_IMPLEMENTATION
@@ -7,15 +7,17 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
+SDL_Surface *IMG_Load_RW(SDL_RWops *src, int freesrc)
+{
   assert(src->type == RW_TYPE_MEM);
   assert(freesrc == 0);
   return NULL;
 }
 
-SDL_Surface* IMG_Load(const char *filename) {
+SDL_Surface *IMG_Load(const char *filename)
+{
   SDL_Surface *s;
-  int fd = open(filename, 0 ,0);
+  int fd = open(filename, 0, 0);
   uint32_t size = lseek(fd, 0, SEEK_END);
   char *img_buf = (char *)malloc(size);
   s = STBIMG_LoadFromMemory(img_buf, size);
@@ -24,14 +26,17 @@ SDL_Surface* IMG_Load(const char *filename) {
   return s;
 }
 
-int IMG_isPNG(SDL_RWops *src) {
+int IMG_isPNG(SDL_RWops *src)
+{
   return 0;
 }
 
-SDL_Surface* IMG_LoadJPG_RW(SDL_RWops *src) {
+SDL_Surface *IMG_LoadJPG_RW(SDL_RWops *src)
+{
   return IMG_Load_RW(src, 0);
 }
 
-char *IMG_GetError() {
+char *IMG_GetError()
+{
   return "Navy does not support IMG_GetError()";
 }
