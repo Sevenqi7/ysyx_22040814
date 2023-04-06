71e1a77ad85c61702844f8dd05d858243f194585
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-06 17:43:30 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-69-generic #76-Ubuntu SMP Fri Mar 17 17:19:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:43:30 up  6:44,  7 users,  load average: 1.36, 1.16, 0.93

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -34,13 +34,13 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
 }
 
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
-  if(!s->format){
+  // if(!s->format){
       if(!x && !y && !w && !h)
           NDL_DrawRect((uint32_t *)s->pixels, 0, 0, s->w, s->h);
       else{
-          NDL_DrawRect((uint32_t *)s->pixels + y * s->w + x, x, y, w, h);
+          // NDL_DrawRect((uint32_t *)s->pixels + y * s->w + x, x, y, w, h);
       }
-  }
+  // }
   // else
   // {
       
