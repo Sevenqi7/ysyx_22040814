7bc6ed96a8cd1bae404cb4ac7e76af61901a0fea
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-06 11:08:21 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-69-generic #76-Ubuntu SMP Fri Mar 17 17:19:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:08:21 up 9 min,  4 users,  load average: 0.43, 0.32, 0.24

--- navy-apps/tests/hsearch-test/hsearch-test.c
+++ navy-apps/tests/hsearch-test/hsearch-test.c
@@ -1,36 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <assert.h>
 #include <search.h>
 
-typedef struct testdata{
-    int a;
-    int b;
-    int c;
-    int d;
-}testdata;
+#define TABLE_SIZE 5
 
 int main()
 {
-  int rets = hcreate(512);
-    testdata *t = malloc(sizeof(testdata));
-    t->a = 1, t->b = 2, t->c = 3, t->d = 4;
-    ENTRY item;
-    item.key = "test";
-    item.data = t;
-    ENTRY *ret = hsearch(item, ENTER);
-    printf("start test\n");
-    if(ret)
-    {
-        ENTRY test;
-        test.key = "test";
-        printf("test.key = %s\n", test.key);
-        ENTRY *again = hsearch(test, FIND);
-        testdata *sb = (testdata *)test.data;
-        printf("testdata: %d %d %d %d\n", sb->a, sb->b, sb->c, sb->d);
+    ENTRY item, *result;
+    ENTRY table[TABLE_SIZE] = {0};
+    hcreate(128);
+    char *key = "hello";
+    char *value = "world";
+
+    // Insert key-value pair into the hash table
+    item.key = key;
+    item.data = value;
+    hsearch(item, ENTER);
+
+    // Search for the key in the hash table
+    item.key = key;
+    result = hsearch(item, FIND);
+
+    // Check if the key was found
+    if (result != NULL) {
+        printf("Value for key '%s' is '%s'\n", result->key, (char*) result->data);
+    } else {
+        printf("Key not found\n");
     }
-    else
-        printf("error\n");
+
     return 0;
 }
