9ee61461306dba2b46ea4e25464fe7b32dada257
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-07 15:34:47 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-69-generic #76-Ubuntu SMP Fri Mar 17 17:19:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:34:47 up  1:18,  3 users,  load average: 0.59, 0.56, 0.78

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -91,11 +91,11 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
     NDL_DrawRect(pixels, 0, 0, s->w, s->h);
   }
   else{
-    printf("untest code 1\n");
+    printf("untest code:2\n");
     pixels = (uint32_t *)malloc(w * h * sizeof(uint32_t));
-    for(int i=0;i<x;i++){
-      for(int j=0;j<y;j++)
-        pixels[i*x + j] = translate_color(&palette[s->pixels[(y+i)*s->w + x + j]]);
+    for(int i=0;i<w;i++){
+      for(int j=0;j<h;j++)
+        pixels[i*w + j] = translate_color(&palette[s->pixels[(y+i)*s->w + x + j]]);
     }
     NDL_DrawRect(pixels, x, y, w, h);
   }
