db91ecbfae62a436e838151623ce4dbdb74ba3be
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-07 18:23:22 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-69-generic #76-Ubuntu SMP Fri Mar 17 17:19:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:23:22 up  4:07,  3 users,  load average: 1.33, 1.01, 0.87

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -27,7 +27,7 @@ switch_boot_pcb();
   Log("Initializing processes...");
 
   // load program here
-  naive_uload(NULL, "/bin/pal");
+  naive_uload(NULL, "/bin/fceux");
 }
 
 Context* schedule(Context *prev) {
