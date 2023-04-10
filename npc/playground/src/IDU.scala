9eab7d10dcb8acd3e5a662dea17c7b10a39ff523
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-10 20:52:35 +0800

    > generate verilog ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-69-generic #76-Ubuntu SMP Fri Mar 17 17:19:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:52:35 up  4:56,  5 users,  load average: 0.87, 1.06, 1.00

--- npc/playground/src/IDU.scala
+++ npc/playground/src/IDU.scala
@@ -79,7 +79,7 @@ class IDU extends Module{
     // immS := Cat(Fill(57, io.IF_Inst(31)), io.IF_Inst(31, 25)) << 5 | io.IF_Inst(11, 7)
     // immB := Cat(Fill(52, io.IF_Inst(31)), ((io.IF_Inst(31) << 11) | (io.IF_Inst(30, 25) << 4) | io.IF_Inst(11, 8) | (io.IF_Inst(7) << 10)))
     // immJ := Cat(Fill(44, io.IF_Inst(31)), (io.IF_Inst(30, 21) | (io.IF_Inst(20) << 10) | (io.IF_Inst(19, 12) << 11) | (io.IF_Inst(31, 31) << 19)))
-    immI := SEXT(io.IF_Inst(31, 20), 32)
+    immI := SEXT(io.IF_Inst(31, 20), 12)
     immU := SEXT(io.IF_Inst(31, 12), 20) << 12
     immS := (SEXT(io.IF_Inst(31, 25), 7) << 5) | io.IF_Inst(11, 7)
     immJ := SEXT(io.IF_Inst(30, 21) | (io.IF_Inst(20) << 10) | (io.IF_Inst(19, 12) << 11) | (io.IF_Inst(31) << 19), 20)
