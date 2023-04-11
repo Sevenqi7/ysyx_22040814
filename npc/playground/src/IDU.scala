93d80f8e6ebc6c0c017feb88450371c2c0bec313
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-11 19:33:39 +0800

    > generate verilog ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-69-generic #76-Ubuntu SMP Fri Mar 17 17:19:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:33:39 up  8:21,  3 users,  load average: 0.92, 0.55, 0.44

--- npc/playground/src/IDU.scala
+++ npc/playground/src/IDU.scala
@@ -163,18 +163,18 @@ class IDU extends Module{
 
     val flush = reset.asBool | io.ID_stall       
 
-    regConnectWithReset(io.ID_pc        , io.IF_pc  , flsuh, 0.U    )
-    regConnectWithReset(io.ID_Inst      , io.IF_Inst, flsuh, 0.U    )
-    regConnectWithReset(io.ID_ALU_Data1 , ALU_Data1 , flsuh, 0.U    )
-    regConnectWithReset(io.ID_RegWriteID, rd        , flsuh, 0.U    )
-    regConnectWithReset(io.ID_ALU_Data2 , ALU_Data2 , flsuh, 0.U    )
-    regConnectWithReset(io.ID_RegWriteEn, RegWriteEn, flsuh, 0.U    )
-    regConnectWithReset(io.ID_MemReadEn , MemReadEn , flsuh, 0.U    )
-    regConnectWithReset(io.ID_MemWriteEn, MemWriteEn, flsuh, 0.U    )
-    regConnectWithReset(io.ID_optype    , opType    , flsuh, 0.U    )
-    regConnectWithReset(io.ID_FuType    , futype    , flsuh, 0.U    )
-    regConnectWithReset(io.ID_Rs1Data   , rs1_data  , flsuh, 0.U    )
-    regConnectWithReset(io.ID_Rs2Data   , rs2_data  , flsuh, 0.U    )
+    regConnectWithReset(io.ID_pc        , io.IF_pc  , flush, 0.U    )
+    regConnectWithReset(io.ID_Inst      , io.IF_Inst, flush, 0.U    )
+    regConnectWithReset(io.ID_ALU_Data1 , ALU_Data1 , flush, 0.U    )
+    regConnectWithReset(io.ID_RegWriteID, rd        , flush, 0.U    )
+    regConnectWithReset(io.ID_ALU_Data2 , ALU_Data2 , flush, 0.U    )
+    regConnectWithReset(io.ID_RegWriteEn, RegWriteEn, flush, 0.U    )
+    regConnectWithReset(io.ID_MemReadEn , MemReadEn , flush, 0.U    )
+    regConnectWithReset(io.ID_MemWriteEn, MemWriteEn, flush, 0.U    )
+    regConnectWithReset(io.ID_optype    , opType    , flush, 0.U    )
+    regConnectWithReset(io.ID_FuType    , futype    , flush, 0.U    )
+    regConnectWithReset(io.ID_Rs1Data   , rs1_data  , flush, 0.U    )
+    regConnectWithReset(io.ID_Rs2Data   , rs2_data  , flush, 0.U    )
 
     val stall_cnt = RegInit(0.U(2.W))
 
