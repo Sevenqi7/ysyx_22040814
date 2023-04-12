fad870b172e034ceffbf3149e94d3727f4e6579a
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-12 11:22:23 +0800

    > generate verilog ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-69-generic #76-Ubuntu SMP Fri Mar 17 17:19:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:22:23 up 43 min,  2 users,  load average: 1.21, 0.77, 0.67

--- npc/playground/src/EXU.scala
+++ npc/playground/src/EXU.scala
@@ -64,8 +64,8 @@ class EXU extends Module{
         (io.EX_MemReadEn.asBool && io.ID_Rs2ID === io.EX_RegWriteID && io.ID_MemWriteEn.asBool, io.MEM_RegWriteData)
         (io.WB_RegWriteEn.asBool && io.WB_RegWriteID === io.ID_Rs2ID && io.ID_MemWriteEn.asBool, io.WB_RegWriteData)
     ))
-    MemWriteData := Mux(io.EX_MemReadEn.asBool && io.ID_Rs2ID === io.EX_RegWriteID && 
-                     io.ID_MemWriteEn.asBool, io.MEM_RegWriteData, io.ID_Rs2Data)
+    // MemWriteData := Mux(io.EX_MemReadEn.asBool && io.ID_Rs2ID === io.EX_RegWriteID && 
+    //                  io.ID_MemWriteEn.asBool, io.MEM_RegWriteData, io.ID_Rs2Data)
     
     regConnect(io.EX_pc             ,         io.ID_pc)
     regConnect(io.EX_Inst           ,       io.ID_Inst)
