08cd19bbae3f0989de55b73ed495f53fdd93c2d3
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-14 21:15:25 +0800

    > generate verilog ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-69-generic #76-Ubuntu SMP Fri Mar 17 17:19:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:15:25 up 12:11,  5 users,  load average: 11.90, 10.25, 9.67

--- npc/playground/src/inst_fetch/pre_fetch.scala
+++ npc/playground/src/inst_fetch/pre_fetch.scala
@@ -16,7 +16,7 @@ class IF_pre_fetch extends Module{
     })
     val axi_lite = IO(new AXILiteMasterIF(32, 64))
     val PF_npc   = RegInit(0x80000004L.U(64.W))
-    PF_npc      := Mux(!io.bp_fail, io.PF_npc+4.U, io.ID_npc+4.U)
+    PF_npc      := Mux(!io.bp_fail, PF_npc+4.U, io.ID_npc+4.U)
 
     io.bp_fail := io.ID_npc =/= io.PF_pc && io.IF_pc =/= 0.U
 
