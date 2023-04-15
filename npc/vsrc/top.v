ed45a87ac37badf2086fd8e2d5436b909cf658fa
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-15 21:01:30 +0800

    > generate verilog ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-69-generic #76-Ubuntu SMP Fri Mar 17 17:19:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:01:30 up  6:28,  3 users,  load average: 0.27, 0.41, 0.49

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1605,7 +1605,6 @@ module top(	// <stdin>:1303:10
     .io_WB_pc                           (io_WB_pc),
     .io_WB_Inst                         (_wb_unit_io_WB_Inst)
   );
-
 wire [63:0] GPR [31:0];
 assign {GPR[31], GPR[30], GPR[29], GPR[28], GPR[27], GPR[26], GPR[25], GPR[24], GPR[23], GPR[22], GPR[21], GPR[20]
 , GPR[19], GPR[18], GPR[17], GPR[16], GPR[15], GPR[14], GPR[13], GPR[12], GPR[11], GPR[10], GPR[9], GPR[8], GPR[7]
@@ -1626,7 +1625,6 @@ sim simulate (	// top.scala:24:26
    .GPR               (GPR),
    .unknown_inst_flag(_inst_decode_unit_io_ID_unknown_inst)
 );
-
   assign io_ID_npc = _inst_decode_unit_io_ID_npc;	// <stdin>:1303:10, top.scala:46:34
   assign io_IF_pc = _inst_fetch_unit_io_IF_pc;	// <stdin>:1303:10, top.scala:45:33
   assign io_ID_pc = _inst_decode_unit_io_ID_to_EX_bus_bits_PC;	// <stdin>:1303:10, top.scala:46:34
@@ -1778,6 +1776,8 @@ endmodule
 import "DPI-C" function void set_gpr_ptr(input logic [63:0] a []);
 import "DPI-C" function void unknown_inst();
 import "DPI-C" function void ebreak(input longint halt_ret);
+
+
 
 
 module sim(input[63:0] IF_pc, input [63:0] GPR [31:0], input unknown_inst_flag, input[31:0] WB_Inst);
