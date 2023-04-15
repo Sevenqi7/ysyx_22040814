c59dfd328866a122e8f7c9ff1a4bf8903df2a421
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-15 10:20:30 +0800

    > generate verilog ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-69-generic #76-Ubuntu SMP Fri Mar 17 17:19:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:20:30 up 11:19,  5 users,  load average: 2.37, 0.91, 0.55

--- npc/playground/src/inst_fetch/pre_fetch.scala
+++ npc/playground/src/inst_fetch/pre_fetch.scala
@@ -20,7 +20,7 @@ class IF_pre_fetch extends Module{
     PF_npc      := Mux(!io.bp_fail, PF_npc+4.U, io.ID_npc)
     io.bp_fail := io.ID_npc =/= (io.PF_pc+4.U) && io.PF_pc =/= 0.U
 
-    regConnectWithResetAndStall(io.PF_pc, Mux(!io.bp_fail, PF_npc, io.ID_npc), reset | io.bp_fail, 0.U(64.W), io.stall)
+    regConnectWithResetAndStall(io.PF_pc, Mux(!io.bp_fail, PF_npc, io.ID_npc), reset.asBool | io.bp_fail, 0.U(64.W), io.stall)
 
     //IFU doesn't write mem
     axi_lite.writeAddr.valid        := 0.U
