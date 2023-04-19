85b919efab407665ec2b97c9f2b0c1ce6349503f
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-19 10:56:58 +0800

    > generate verilog ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-69-generic #76-Ubuntu SMP Fri Mar 17 17:19:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:56:58 up 23 min,  4 users,  load average: 0.49, 0.54, 0.53

--- npc/playground/src/mem_unit/MEMU.scala
+++ npc/playground/src/mem_unit/MEMU.scala
@@ -39,66 +39,34 @@ class MEM_to_WB_Message extends Bundle{
 
 class MEMU extends Module{
     val io = IO(new Bundle{
-        val EX_to_MEM_bus = Flipped(Decoupled(new EX_MEM_Message))
+        val PMEM_to_MEM_bus = Flipped(Decoupled(new PMEM_MEM_Message))
         val MEM_to_WB_bus = Decoupled(new MEM_to_WB_Message)
         val MEM_to_ID_forward = Decoupled(new MEM_to_ID_Message)
-
-        //for NPC to trace
-        val PMEM_pc = Output(UInt(64.W))
     })
 
-    //unpack bus from EXU
-
-
-    val pre_mem      =  Module(new MEM_pre_stage)
-    val data_ram     =  Module(new sim_sram)
-    io.PMEM_pc       :=  pre_mem.io.PMEM_to_MEM_bus.bits.PC
-
-    pre_mem.io.EX_to_MEM_bus := io.EX_to_MEM_bus
-
-    data_ram.io.pc      := pre_mem.io.PMEM_to_MEM_bus.bits.PC
-    data_ram.io.aclk    := clock
-    data_ram.io.aresetn := !reset.asBool
-    //ar
-    data_ram.io.araddr  := pre_mem.axi_lite.readAddr.bits.addr
-    data_ram.io.arvalid := pre_mem.axi_lite.readAddr.valid
-    pre_mem.axi_lite.readAddr.ready     := data_ram.io.arready
-    //r
-    pre_mem.axi_lite.readData.bits.data := data_ram.io.rdata
-    pre_mem.axi_lite.readData.bits.resp := data_ram.io.rresp
-    pre_mem.axi_lite.readData.valid     := data_ram.io.rvalid
-    data_ram.io.rready                  := pre_mem.axi_lite.readData.ready
-    //aw
-    data_ram.io.awaddr                  := pre_mem.axi_lite.writeAddr.bits.addr
-    data_ram.io.awvalid                 := pre_mem.axi_lite.writeAddr.valid
-    pre_mem.axi_lite.writeAddr.ready    := data_ram.io.awready
-    //w
-    data_ram.io.wdata                   := pre_mem.axi_lite.writeData.bits.data
-    data_ram.io.wstrb                   := pre_mem.axi_lite.writeData.bits.strb
-    data_ram.io.wvalid                  := pre_mem.axi_lite.writeData.valid
-    pre_mem.axi_lite.writeData.ready    := data_ram.io.wready
-    //b
-    pre_mem.axi_lite.writeResp.bits.resp  := data_ram.io.bresp
-    pre_mem.axi_lite.writeResp.valid      := data_ram.io.bvalid
-    data_ram.io.bready                    := pre_mem.axi_lite.writeResp.ready
-
+    //unpack bus from PMEMU
+    val PMEM_pc    = io.PMEM_to_MEM_bus.bits.PC
+    val PMEM_Inst  = io.PMEM_to_MEM_bus.bits.Inst
+    val ALU_result = io.PMEM_to_MEM_bus.bits.ALU_result
+    val regWriteEn = io.PMEM_to_MEM_bus.bits.regWriteEn
+    val regWriteID = io.PMEM_to_MEM_bus.bits.regWriteID
+    val memReadEn  = io.PMEM_to_MEM_bus.bits.memReadEn
+    val lsutype    = io.PMEM_to_MEM_bus.bits.lsutype
 
     val regWriteData = Wire(UInt(64.W))
 
-    regWriteData := Mux(pre_mem.io.PMEM_to_MEM_bus.bits.memReadEn, pre_mem.io.memReadData, 
-                                            pre_mem.io.PMEM_to_MEM_bus.bits.ALU_result)
+    regWriteData := Mux(memReadEn, memReadData, ALU_result)
     
-    regConnect(io.MEM_to_WB_bus.bits.PC                , pre_mem.io.PMEM_to_MEM_bus.bits.PC   )
-    regConnect(io.MEM_to_WB_bus.bits.Inst              , pre_mem.io.PMEM_to_MEM_bus.bits.Inst   )
-    regConnect(io.MEM_to_WB_bus.bits.regWriteEn        , pre_mem.io.PMEM_to_MEM_bus.bits.regWriteEn   )
-    regConnect(io.MEM_to_WB_bus.bits.regWriteID        , pre_mem.io.PMEM_to_MEM_bus.bits.regWriteID    )
-    regConnect(io.MEM_to_WB_bus.bits.regWriteData      , regWriteData)
+    regConnect(io.MEM_to_WB_bus.bits.PC                , PMEM_pc        )
+    regConnect(io.MEM_to_WB_bus.bits.Inst              , PMEM_Inst      )
+    regConnect(io.MEM_to_WB_bus.bits.regWriteEn        , regWriteEn     )
+    regConnect(io.MEM_to_WB_bus.bits.regWriteID        , regWriteID     )
+    regConnect(io.MEM_to_WB_bus.bits.regWriteData      , regWriteData   )
     regConnect(io.MEM_to_WB_bus.valid                  , pre_mem.io.PMEM_to_MEM_bus.valid)
-    pre_mem.io.PMEM_to_MEM_bus.ready       := 1.U    
+    io.PMEM_to_MEM_bus.ready               := 1.U
 
     io.MEM_to_ID_forward.bits.regWriteData := regWriteData
-    io.MEM_to_ID_forward.bits.regWriteEn   := pre_mem.io.PMEM_to_MEM_bus.bits.regWriteEn
-    io.MEM_to_ID_forward.bits.regWriteID   := pre_mem.io.PMEM_to_MEM_bus.bits.regWriteID
-    io.MEM_to_ID_forward.valid             := pre_mem.io.PMEM_to_MEM_bus.bits.regWriteEn & (pre_mem.io.PMEM_to_MEM_bus.bits.regWriteID > 0.U)
-
+    io.MEM_to_ID_forward.bits.regWriteEn   := regWriteEn
+    io.MEM_to_ID_forward.bits.regWriteID   := regWriteID
+    io.MEM_to_ID_forward.valid             := regWriteEn & (regWriteID > 0.U)
 }  
