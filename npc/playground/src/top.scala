638b31b2911321314f52a4e9b37d4388e9caffb2
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-20 21:10:35 +0800

    > generate verilog ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-70-generic #77-Ubuntu SMP Tue Mar 21 14:02:37 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:10:35 up 1 day,  3:23,  5 users,  load average: 3.85, 3.40, 3.24

--- npc/playground/src/top.scala
+++ npc/playground/src/top.scala
@@ -53,7 +53,6 @@ class top extends Module{
     val wb_unit = Module(new WBU)
 
     val inst_ram     = Module(new sim_sram)
-    val data_ram     = Module(new sim_sram)
 
     io.IF_Inst  := inst_fetch_unit.io.IF_to_ID_bus.bits.Inst
     io.IF_valid := inst_fetch_unit.io.IF_to_ID_bus.valid
@@ -85,7 +84,33 @@ class top extends Module{
     simulate.io.GPR                         := inst_decode_unit.io.ID_GPR
     simulate.io.WB_Inst                     := wb_unit.io.WB_Inst
     simulate.io.unknown_inst_flag           := inst_decode_unit.io.ID_unknown_inst
+
+    inst_fetch_unit.io.ID_npc               := inst_decode_unit.io.ID_npc
     
+    inst_decode_unit.io.IF_to_ID_bus        <> inst_fetch_unit.io.IF_to_ID_bus
+    inst_decode_unit.io.WB_to_ID_forward    <> wb_unit.io.WB_to_ID_forward
+    inst_decode_unit.io.PMEM_to_ID_forward  <> pre_mem_unit.io.PMEM_to_ID_forward
+    inst_decode_unit.io.MEM_to_ID_forward   <> mem_unit.io.MEM_to_ID_forward
+    inst_decode_unit.io.EX_ALUResult        := excute_unit.io.EX_ALUResult_Pass
+
+    excute_unit.io.ID_to_EX_bus             <> inst_decode_unit.io.ID_to_EX_bus
+    excute_unit.io.MEM_regWriteData         := mem_unit.io.MEM_to_ID_forward.bits.regWriteData
+    excute_unit.io.WB_to_EX_forward         <> wb_unit.io.WB_to_ID_forward
+
+    //PMEM
+    pre_mem_unit.io.EX_to_MEM_bus           <> excute_unit.io.EX_to_MEM_bus
+
+    //PMEM END
+
+    mem_unit.io.PMEM_to_MEM_bus             <> pre_mem_unit.io.PMEM_to_MEM_bus
+    mem_unit.io.memReadData                 := pre_mem_unit.io.memReadData
+
+    wb_unit.io.MEM_to_WB_bus                <> mem_unit.io.MEM_to_WB_bus
+
+
+
+
+    //inst ram
 
     //axi-lite
     inst_ram.io.pc                          := inst_fetch_unit.io.PF_pc
@@ -114,52 +139,40 @@ class top extends Module{
     inst_fetch_unit.axi_lite.writeResp.valid      := inst_ram.io.bready
     inst_ram.io.bready                      := inst_fetch_unit.axi_lite.writeResp.ready
 
+    val arb = Module(new AXI_Arbiter)
+    arb.axi_lite <> pre_mem_unit.axi_lite
+}
 
-    inst_fetch_unit.io.ID_npc               := inst_decode_unit.io.ID_npc
-    
-    inst_decode_unit.io.IF_to_ID_bus        <> inst_fetch_unit.io.IF_to_ID_bus
-    inst_decode_unit.io.WB_to_ID_forward    <> wb_unit.io.WB_to_ID_forward
-    inst_decode_unit.io.PMEM_to_ID_forward  <> pre_mem_unit.io.PMEM_to_ID_forward
-    inst_decode_unit.io.MEM_to_ID_forward   <> mem_unit.io.MEM_to_ID_forward
-    inst_decode_unit.io.EX_ALUResult        := excute_unit.io.EX_ALUResult_Pass
+class AXI_Arbiter extends Module{
+    val axi_lite = IO(Flipped(new AXILiteMasterIF(32, 64)))
+    val data_ram     = Module(new sim_sram)
 
-    excute_unit.io.ID_to_EX_bus             <> inst_decode_unit.io.ID_to_EX_bus
-    excute_unit.io.MEM_regWriteData         := mem_unit.io.MEM_to_ID_forward.bits.regWriteData
-    excute_unit.io.WB_to_EX_forward         <> wb_unit.io.WB_to_ID_forward
-
-    //PMEM
-    pre_mem_unit.io.EX_to_MEM_bus           <> excute_unit.io.EX_to_MEM_bus
-    data_ram.io.pc                          := pre_mem_unit.io.PMEM_to_MEM_bus.bits.PC
+    //data ram
+    data_ram.io.pc                          := 0.U
 
     data_ram.io.aclk                        := clock
     data_ram.io.aresetn                     := !reset.asBool
     //ar
-    data_ram.io.araddr                      := pre_mem_unit.axi_lite.readAddr.bits.addr
-    data_ram.io.arvalid                     := pre_mem_unit.axi_lite.readAddr.valid
-    pre_mem_unit.axi_lite.readAddr.ready    := data_ram.io.arready
+    data_ram.io.araddr                      := axi_lite.readAddr.bits.addr
+    data_ram.io.arvalid                     := axi_lite.readAddr.valid
+    axi_lite.readAddr.ready    := data_ram.io.arready
     //r
-    pre_mem_unit.axi_lite.readData.bits.data := data_ram.io.rdata
-    pre_mem_unit.axi_lite.readData.bits.resp := data_ram.io.rresp
-    pre_mem_unit.axi_lite.readData.valid     := data_ram.io.rvalid
-    data_ram.io.rready                      := pre_mem_unit.axi_lite.readData.ready
+    axi_lite.readData.bits.data := data_ram.io.rdata
+    axi_lite.readData.bits.resp := data_ram.io.rresp
+    axi_lite.readData.valid     := data_ram.io.rvalid
+    data_ram.io.rready                      := axi_lite.readData.ready
     //aw
-    data_ram.io.awaddr                      := pre_mem_unit.axi_lite.writeAddr.bits.addr
-    data_ram.io.awvalid                     := pre_mem_unit.axi_lite.writeAddr.valid
-    pre_mem_unit.axi_lite.writeAddr.ready   := data_ram.io.awready
+    data_ram.io.awaddr                      := axi_lite.writeAddr.bits.addr
+    data_ram.io.awvalid                     := axi_lite.writeAddr.valid
+    axi_lite.writeAddr.ready   := data_ram.io.awready
     //w
-    data_ram.io.wdata                       := pre_mem_unit.axi_lite.writeData.bits.data
-    data_ram.io.wstrb                       := pre_mem_unit.axi_lite.writeData.bits.strb
-    data_ram.io.wvalid                      := pre_mem_unit.axi_lite.writeData.valid
-    pre_mem_unit.axi_lite.writeData.ready        := data_ram.io.wready
+    data_ram.io.wdata                       := axi_lite.writeData.bits.data
+    data_ram.io.wstrb                       := axi_lite.writeData.bits.strb
+    data_ram.io.wvalid                      := axi_lite.writeData.valid
+    axi_lite.writeData.ready        := data_ram.io.wready
     //b
-    pre_mem_unit.axi_lite.writeResp.bits.resp := data_ram.io.bresp
-    pre_mem_unit.axi_lite.writeResp.valid     := data_ram.io.bvalid
-    data_ram.io.bready                      := pre_mem_unit.axi_lite.writeResp.ready
-    //PMEM END
-
-    mem_unit.io.PMEM_to_MEM_bus             <> pre_mem_unit.io.PMEM_to_MEM_bus
-    mem_unit.io.memReadData                 := pre_mem_unit.io.memReadData
-
-    wb_unit.io.MEM_to_WB_bus                <> mem_unit.io.MEM_to_WB_bus
+    axi_lite.writeResp.bits.resp := data_ram.io.bresp
+    axi_lite.writeResp.valid     := data_ram.io.bvalid
+    data_ram.io.bready                      := axi_lite.writeResp.ready
 
 }
