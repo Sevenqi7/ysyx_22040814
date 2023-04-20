898ef9110a71ae7cf3d3089452eb74151d82ac94
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-20 19:45:26 +0800

    > generate verilog ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-70-generic #77-Ubuntu SMP Tue Mar 21 14:02:37 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:45:26 up 1 day,  1:58,  5 users,  load average: 2.57, 2.65, 2.60

--- npc/playground/src/utils/AXILiteDefs.scala
+++ npc/playground/src/utils/AXILiteDefs.scala
@@ -30,7 +30,7 @@ package AXILiteDefs
     // write data channel
     val writeData   = Flipped(Decoupled(new AXILiteWriteData(dataWidth)))
     // write response channel (for memory consistency)
-    val writeResp   = Decoupled(UInt(2.W))
+    val writeResp   = Decoupled(new AXILiteWriteResp)
     
     // read address channel
     val readAddr    = Flipped(Decoupled(new AXILiteAddress(addrWidth)))
