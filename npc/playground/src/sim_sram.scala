43c9875445485fcb41f55c4b935d79cc9dae6f76
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-20 19:42:32 +0800

    > generate verilog ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-70-generic #77-Ubuntu SMP Tue Mar 21 14:02:37 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:42:32 up 1 day,  1:55,  5 users,  load average: 2.68, 2.52, 2.54

--- npc/playground/src/sim_sram.scala
+++ npc/playground/src/sim_sram.scala
@@ -57,28 +57,32 @@ class RAMU extends Module{
     arb.io.axi_IF                               <> io.axi_IF
     arb.io.axi_MEM                              <> io.axi_MEM
 
-    data_ram.io.aclk                            := clock
-    data_ram.io.aresetn                         := !reset.asBool
-    //ar
-    data_ram.io.araddr                          := axi_sel.bits.readAddr.bits.addr
-    data_ram.io.arvalid                         := axi_sel.bits.readAddr.valid
-    axi_sel.bits.readAddr.ready      := data_ram.io.arready
-    //r
-    axi_sel.bits.readData.bits.data  := data_ram.io.rdata
-    axi_sel.bits.readData.bits.resp  := data_ram.io.rresp
-    axi_sel.bits.readData.valid      := data_ram.io.rvalid
-    data_ram.io.rready                          := axi_sel.bits.readData.ready
-    //aw
-    data_ram.io.awaddr                          := axi_sel.bits.writeAddr.bits.addr
-    data_ram.io.awvalid                         := axi_sel.bits.writeAddr.valid
-    axi_sel.bits.writeAddr.ready     := data_ram.io.awready
-    //w
-    data_ram.io.wdata                           := axi_sel.bits.writeData.bits.data
-    data_ram.io.wstrb                           := axi_sel.bits.writeData.bits.strb
-    data_ram.io.wvalid                          := axi_sel.bits.writeData.valid
-    axi_sel.bits.writeData.ready     := data_ram.io.wready
-    //b
-    axi_sel.bits.writeResp.bits.resp := data_ram.io.bresp
-    axi_sel.bits.writeResp.valid     := data_ram.io.bvalid
-    data_ram.io.bready                          := axi_sel.bits.writeResp.ready
+    val axi_sram = new AXILiteSlaveIF(32, 64)
+    
+    axi_sram    <> axi_sel
+
+    // data_ram.io.aclk                            := clock
+    // data_ram.io.aresetn                         := !reset.asBool
+    // //ar
+    // data_ram.io.araddr                          := axi_sel.bits.readAddr.bits.addr
+    // data_ram.io.arvalid                         := axi_sel.bits.readAddr.valid
+    // axi_sel.bits.readAddr.ready                 := data_ram.io.arready
+    // //r
+    // axi_sel.bits.readData.bits.data             := data_ram.io.rdata
+    // axi_sel.bits.readData.bits.resp             := data_ram.io.rresp
+    // axi_sel.bits.readData.valid                 := data_ram.io.rvalid
+    // data_ram.io.rready                          := axi_sel.bits.readData.ready
+    // //aw
+    // data_ram.io.awaddr                          := axi_sel.bits.writeAddr.bits.addr
+    // data_ram.io.awvalid                         := axi_sel.bits.writeAddr.valid
+    // axi_sel.bits.writeAddr.ready                := data_ram.io.awready
+    // //w
+    // data_ram.io.wdata                           := axi_sel.bits.writeData.bits.data
+    // data_ram.io.wstrb                           := axi_sel.bits.writeData.bits.strb
+    // data_ram.io.wvalid                          := axi_sel.bits.writeData.valid
+    // axi_sel.bits.writeData.ready     := data_ram.io.wready
+    // //b
+    // axi_sel.bits.writeResp.bits.resp := data_ram.io.bresp
+    // axi_sel.bits.writeResp.valid     := data_ram.io.bvalid
+    // data_ram.io.bready                          := axi_sel.bits.writeResp.ready
 }
