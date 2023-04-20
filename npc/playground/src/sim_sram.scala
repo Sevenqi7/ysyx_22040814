029f4ced74fdb4f708eaf52c1ef0b44764c911aa
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-20 19:57:41 +0800

    > generate verilog ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-70-generic #77-Ubuntu SMP Tue Mar 21 14:02:37 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:57:41 up 1 day,  2:11,  5 users,  load average: 2.97, 2.60, 2.53

--- npc/playground/src/sim_sram.scala
+++ npc/playground/src/sim_sram.scala
@@ -33,56 +33,63 @@ class sim_sram extends BlackBox with HasBlackBoxPath{
     addPath("/home/seven7/Documents/学业/一生一芯/ysyx-workbench/npc/playground/verilog/sim_sram.v")
 }
 
-class AXI_Arbiter extends Module{
-    val io = IO(new Bundle{
-        val axi_IF  = Flipped(Decoupled(new AXILiteMasterIF(32, 64)))
-        val axi_MEM = Flipped(Decoupled(new AXILiteMasterIF(32, 64)))
-        val axi_out = Decoupled(new AXILiteMasterIF(32, 64))
-    })
-
-    val arbiter = Module(new Arbiter(new AXILiteMasterIF(32, 64), 2))
-    arbiter.io.in(0) <> io.axi_IF
-    arbiter.io.in(1) <> io.axi_MEM
-    io.axi_out       <> arbiter.io.out
-}
-
 class RAMU extends Module{
     val io = IO(new Bundle{
         val axi_IF  = Flipped(Decoupled(new AXILiteMasterIF(32, 64)))
         val axi_MEM = Flipped(Decoupled(new AXILiteMasterIF(32, 64)))  
     })
     val data_ram = Module(new sim_sram())
-    val arb = Module(new AXI_Arbiter)
-    val axi_sel = arb.io.axi_out
-    arb.io.axi_IF                               <> io.axi_IF
-    arb.io.axi_MEM                              <> io.axi_MEM
+    data_ram.io.aclk                            := clock
+    data_ram.io.aresetn                         := !reset.asBool
 
-    val axi_sram = Wire(Decoupled(new AXILiteSlaveIF(32, 64)))
-    
-    axi_sram    <> axi_sel
+    when(io.axi_MEM.valid)
+    {
+        //ar
+        data_ram.io.araddr                          := io.axi_MEM.bits.readAddr.bits.addr
+        data_ram.io.arvalid                         := io.axi_MEM.bits.readAddr.valid
+        io.axi_MEM.bits.readAddr.ready              := data_ram.io.arready
+        //r
+        io.axi_MEM.bits.readData.bits.data          := data_ram.io.rdata
+        io.axi_MEM.bits.readData.bits.resp          := data_ram.io.rresp
+        io.axi_MEM.bits.readData.valid              := data_ram.io.rvalid
+        data_ram.io.rready                          := io.axi_MEM.bits.readData.ready
+        //aw
+        data_ram.io.awaddr                          := io.axi_MEM.bits.writeAddr.bits.addr
+        data_ram.io.awvalid                         := io.axi_MEM.bits.writeAddr.valid
+        io.axi_MEM.bits.writeAddr.ready             := data_ram.io.awready
+        //w
+        data_ram.io.wdata                           := io.axi_MEM.bits.writeData.bits.data
+        data_ram.io.wstrb                           := io.axi_MEM.bits.writeData.bits.strb
+        data_ram.io.wvalid                          := io.axi_MEM.bits.writeData.valid
+        io.axi_MEM.bits.writeData.ready             := data_ram.io.wready  
+        //b
+        io.axi_MEM.bits.writeResp.bits.resp         := data_ram.io.bresp
+        io.axi_MEM.bits.writeResp.valid             := data_ram.io.bvalid
+        data_ram.io.bready                          := io.axi_MEM.bits.writeResp.ready
+    }
+    .otherwise{
+        //ar
+        data_ram.io.araddr                          := io.axi_IF.bits.readAddr.bits.addr
+        data_ram.io.arvalid                         := io.axi_IF.bits.readAddr.valid
+        io.axi_IF.bits.readAddr.ready              := data_ram.io.arready
+        //r
+        io.axi_IF.bits.readData.bits.data          := data_ram.io.rdata
+        io.axi_IF.bits.readData.bits.resp          := data_ram.io.rresp
+        io.axi_IF.bits.readData.valid              := data_ram.io.rvalid
+        data_ram.io.rready                          := io.axi_IF.bits.readData.ready
+        //aw
+        data_ram.io.awaddr                          := io.axi_IF.bits.writeAddr.bits.addr
+        data_ram.io.awvalid                         := io.axi_IF.bits.writeAddr.valid
+        io.axi_IF.bits.writeAddr.ready             := data_ram.io.awready
+        //w
+        data_ram.io.wdata                           := io.axi_IF.bits.writeData.bits.data
+        data_ram.io.wstrb                           := io.axi_IF.bits.writeData.bits.strb
+        data_ram.io.wvalid                          := io.axi_IF.bits.writeData.valid
+        io.axi_IF.bits.writeData.ready             := data_ram.io.wready  
+        //b
+        io.axi_IF.bits.writeResp.bits.resp         := data_ram.io.bresp
+        io.axi_IF.bits.writeResp.valid             := data_ram.io.bvalid
+        data_ram.io.bready                          := io.axi_IF.bits.writeResp.ready
+    }
 
-    // data_ram.io.aclk                            := clock
-    // data_ram.io.aresetn                         := !reset.asBool
-    // //ar
-    // data_ram.io.araddr                          := axi_sel.bits.readAddr.bits.addr
-    // data_ram.io.arvalid                         := axi_sel.bits.readAddr.valid
-    // axi_sel.bits.readAddr.ready                 := data_ram.io.arready
-    // //r
-    // axi_sel.bits.readData.bits.data             := data_ram.io.rdata
-    // axi_sel.bits.readData.bits.resp             := data_ram.io.rresp
-    // axi_sel.bits.readData.valid                 := data_ram.io.rvalid
-    // data_ram.io.rready                          := axi_sel.bits.readData.ready
-    // //aw
-    // data_ram.io.awaddr                          := axi_sel.bits.writeAddr.bits.addr
-    // data_ram.io.awvalid                         := axi_sel.bits.writeAddr.valid
-    // axi_sel.bits.writeAddr.ready                := data_ram.io.awready
-    // //w
-    // data_ram.io.wdata                           := axi_sel.bits.writeData.bits.data
-    // data_ram.io.wstrb                           := axi_sel.bits.writeData.bits.strb
-    // data_ram.io.wvalid                          := axi_sel.bits.writeData.valid
-    // axi_sel.bits.writeData.ready     := data_ram.io.wready
-    // //b
-    // axi_sel.bits.writeResp.bits.resp := data_ram.io.bresp
-    // axi_sel.bits.writeResp.valid     := data_ram.io.bvalid
-    // data_ram.io.bready                          := axi_sel.bits.writeResp.ready
 }
