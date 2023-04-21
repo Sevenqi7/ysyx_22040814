396f1b9cef416c0a6e83a88ecad1ba436718dd75
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-21 21:26:34 +0800

    > generate verilog ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-70-generic #77-Ubuntu SMP Tue Mar 21 14:02:37 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:26:34 up 21:20,  4 users,  load average: 1.70, 1.34, 1.21

--- npc/playground/src/inst_fetch/pre_fetch.scala
+++ npc/playground/src/inst_fetch/pre_fetch.scala
@@ -20,21 +20,22 @@ class IF_pre_fetch extends Module{
     val axi_req  = IO(new MyReadyValidIO)
     val PF_npc   = RegInit(0x80000000L.U(64.W))
     
-    axi_req.valid   := 1.U
     val axi_busy = RegInit(0.U(1.W))
     axi_busy := !axi_req.ready
-
-    io.PF_npc    := PF_npc
-    PF_npc      := MuxCase(io.PF_npc+4.U, Seq(
-        (io.bp_fail , io.ID_npc),
-        (io.stall | !axi_req.ready | axi_busy ,   io.PF_npc)
-    ))
-
     io.bp_fail := io.ID_npc =/= io.PF_pc && io.PF_pc =/= 0.U && io.IF_pc =/= 0.U && !io.stall
     val bp_fail_r = RegInit(0.U(1.W))
     bp_fail_r := io.bp_fail
 
-    regConnectWithResetAndStall(io.PF_pc, PF_npc, reset.asBool | io.bp_fail, 0.U(64.W), io.stall | !axi_req.ready | axi_busy)
+    axi_req.valid   := 1.U
+    
+    io.PF_npc    := PF_npc
+    PF_npc      := MuxCase(io.PF_npc+4.U, Seq(
+        (io.bp_fail , io.ID_npc),
+        (io.stall | !axi_req.ready | axi_busy.asBool ,   io.PF_npc)
+    ))
+
+    
+    regConnectWithResetAndStall(io.PF_pc, PF_npc, reset.asBool | io.bp_fail, 0.U(64.W), io.stall | !axi_req.ready | axi_busy.asBool)
 
     //IFU doesn't write mem
     axi_lite.writeAddr.valid        := 0.U
