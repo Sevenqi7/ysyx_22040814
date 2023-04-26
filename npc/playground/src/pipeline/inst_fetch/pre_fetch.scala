cc93ed0da2d42cd9e9cd080c60c240e91ff10604
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-26 22:06:26 +0800

    > generate verilog ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-70-generic #77-Ubuntu SMP Tue Mar 21 14:02:37 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:06:26 up  3:47,  4 users,  load average: 0.85, 0.59, 0.63

--- npc/playground/src/pipeline/inst_fetch/pre_fetch.scala
+++ npc/playground/src/pipeline/inst_fetch/pre_fetch.scala
@@ -35,9 +35,9 @@ class IF_pre_fetch extends Module{
 
     axi_req.valid   := 1.U
     PF_npc := MuxCase(io.PF_npc + 4.U, Seq(
-        (io.bp_flush              , io.bp_npc      ),
-        (io.bp_taken              , io.bp_npc + 4.U),
-        (io.stall | !axi_req.ready, io.PF_npc      )
+        (io.bp_flush | io.bp_taken, io.bp_npc      ),
+        (io.bp_fail_r             , io.PF_npc      ),
+        (io.stall | !axi_req.ready, io.PF_pc       )
     ))
     // PF_npc      := MuxCase(io.PF_npc+4.U, Seq(
     //     (io.bp_fail, io.ID_npc),
