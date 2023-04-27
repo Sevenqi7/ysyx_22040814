4385160f0637ca1714cfee7d6a3b3c787f98f94f
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-27 16:49:40 +0800

    > generate verilog ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-71-generic #78-Ubuntu SMP Tue Apr 18 09:00:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:49:40 up  2:16,  4 users,  load average: 1.13, 0.81, 0.69

--- npc/playground/src/pipeline/inst_fetch/pre_fetch.scala
+++ npc/playground/src/pipeline/inst_fetch/pre_fetch.scala
@@ -49,7 +49,7 @@ class IF_pre_fetch extends Module{
     val npc = Wire(UInt(64.W))
     npc := MuxCase(PF_npc, Seq(
         (io.bp_taken | io.bp_flush, io.bp_npc),
-        (io.stall    | !axo_req.ready, io.PF_pc)
+        (io.stall    | !axi_req.ready, io.PF_pc)
     ))
     regConnectWithResetAndStall(io.PF_pc, npc, reset.asBool, 0.U(64.W), io.stall | !axi_req.ready)
 
