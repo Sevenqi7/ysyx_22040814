7df8c8ab8f5975c6cdd54ea9b9ad5e83d1a72f1c
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-28 03:11:19 +0800

    > generate verilog ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-71-generic #78-Ubuntu SMP Tue Apr 18 09:00:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  03:11:19 up  3:26,  6 users,  load average: 1.14, 0.68, 0.61

--- npc/playground/src/pipeline/inst_fetch/bpu.scala
+++ npc/playground/src/pipeline/inst_fetch/bpu.scala
@@ -186,8 +186,8 @@ class BPU extends Module{
         
     //BHT & PHT
     //1.prediction
-    // val bht_idx = hash(io.PF_pc)
-    val bht_idx = io.PF_pc(phtIdxWidth + bhtIdxWidth -1, phtIdxWidth)
+    val bht_idx = hash(io.PF_pc)
+    // val bht_idx = io.PF_pc(phtIdxWidth + bhtIdxWidth -1, phtIdxWidth)
     val pht_idx = io.PF_pc(phtIdxWidth-1, 0)
 
     bp_taken     := 0.U
@@ -197,8 +197,8 @@ class BPU extends Module{
 
     
     //2.update
-    // val up_bht_idx = hash(ID_pc)
-    val up_bht_idx = ID_pc(phtIdxWidth + bhtIdxWidth -1, phtIdxWidth)
+    val up_bht_idx = hash(ID_pc)
+    // val up_bht_idx = ID_pc(phtIdxWidth + bhtIdxWidth -1, phtIdxWidth)
     val up_pht_idx = ID_pc(phtIdxWidth-1, 0)
     when(io.ID_to_BPU_bus.valid){
         
