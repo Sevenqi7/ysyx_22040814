4f47bd2b9d10d08147cded705ea1c2c8deeda1a3
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-30 18:52:54 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-71-generic #78-Ubuntu SMP Tue Apr 18 09:00:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:52:54 up 53 min,  5 users,  load average: 1.46, 1.06, 0.89

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -13,42 +13,42 @@
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
 size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 
-static uintptr_t loader(PCB *pcb, const char *filename) {
-  // TODO();
-  Elf_Ehdr eh;
-  Elf_Phdr ph;
-  ramdisk_read(&eh, 0, sizeof(Elf_Ehdr));
-  assert(*((uint32_t *)eh.e_ident) == 0x464c457f);
-  for(int i=0;i<eh.e_phnum;i++)
-  {
-    ramdisk_read(&ph, eh.e_phoff + i * sizeof(Elf_Phdr), sizeof(Elf_Phdr));
-    ramdisk_read((uintptr_t *)ph.p_vaddr ,ph.p_offset, ph.p_memsz);
-    Log("Segment %d: vaddr:0x%lx size:%d", i, ph.p_vaddr, ph.p_memsz);
-    memset((uintptr_t *)(ph.p_vaddr + ph.p_filesz), 0, ph.p_memsz - ph.p_filesz);
-  }
-  return eh.e_entry;
-}
-
 // static uintptr_t loader(PCB *pcb, const char *filename) {
-//   int fd = fs_open(filename, 0, 0);
-//   Log("loader load file:%s", filename);
-//   assert(fd != -1);
+//   // TODO();
 //   Elf_Ehdr eh;
 //   Elf_Phdr ph;
-//   fs_read(fd, &eh, sizeof(Elf_Ehdr));
+//   ramdisk_read(&eh, 0, sizeof(Elf_Ehdr));
 //   assert(*((uint32_t *)eh.e_ident) == 0x464c457f);
 //   for(int i=0;i<eh.e_phnum;i++)
 //   {
-//     fs_lseek(fd, eh.e_phoff + i * sizeof(Elf_Phdr), SEEK_SET);
-//     fs_read(fd, &ph, sizeof(Elf_Phdr));
-//     // Log("Segment %d: vaddr:0x%lx size:%d", i, ph.p_vaddr, ph.p_memsz);
-//     fs_lseek(fd, ph.p_offset, SEEK_SET);
-//     fs_read(fd, (void *)ph.p_vaddr, ph.p_memsz);
+//     ramdisk_read(&ph, eh.e_phoff + i * sizeof(Elf_Phdr), sizeof(Elf_Phdr));
+//     ramdisk_read((uintptr_t *)ph.p_vaddr ,ph.p_offset, ph.p_memsz);
+//     Log("Segment %d: vaddr:0x%lx size:%d", i, ph.p_vaddr, ph.p_memsz);
 //     memset((uintptr_t *)(ph.p_vaddr + ph.p_filesz), 0, ph.p_memsz - ph.p_filesz);
 //   }
-//   fs_close(fd);
 //   return eh.e_entry;
 // }
+
+static uintptr_t loader(PCB *pcb, const char *filename) {
+  int fd = fs_open(filename, 0, 0);
+  Log("loader load file:%s", filename);
+  assert(fd != -1);
+  Elf_Ehdr eh;
+  Elf_Phdr ph;
+  fs_read(fd, &eh, sizeof(Elf_Ehdr));
+  assert(*((uint32_t *)eh.e_ident) == 0x464c457f);
+  for(int i=0;i<eh.e_phnum;i++)
+  {
+    fs_lseek(fd, eh.e_phoff + i * sizeof(Elf_Phdr), SEEK_SET);
+    fs_read(fd, &ph, sizeof(Elf_Phdr));
+    // Log("Segment %d: vaddr:0x%lx size:%d", i, ph.p_vaddr, ph.p_memsz);
+    fs_lseek(fd, ph.p_offset, SEEK_SET);
+    fs_read(fd, (void *)ph.p_vaddr, ph.p_memsz);
+    memset((uintptr_t *)(ph.p_vaddr + ph.p_filesz), 0, ph.p_memsz - ph.p_filesz);
+  }
+  fs_close(fd);
+  return eh.e_entry;
+}
 
 void naive_uload(PCB *pcb, const char *filename) {
   uintptr_t entry = loader(pcb, filename);
