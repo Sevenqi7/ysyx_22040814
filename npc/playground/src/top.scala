66760b9844aa7e7c345777fb8d95401961d414f3
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-30 18:19:59 +0800

    > generate verilog ysyx_22040814 蒋晓天 Linux seven7-Lenovo-ThinkBook-14p-Gen-2 5.15.0-71-generic #78-Ubuntu SMP Tue Apr 18 09:00:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:19:59 up 20 min,  5 users,  load average: 0.29, 0.53, 0.61

--- npc/playground/src/top.scala
+++ npc/playground/src/top.scala
@@ -71,6 +71,7 @@ class top extends Module{
 
         val ID_ALU_Data1 = Output(UInt(64.W))
         val ID_ALU_Data2 = Output(UInt(64.W))
+        val EX_ALU_result_pass = Output(UInt(64.W))
         val ID_Rs1Data = Output(UInt(64.W))
         val ID_Rs2Data = Output(UInt(64.W))
         val ALUResult = Output(UInt(64.W))
@@ -138,6 +139,7 @@ class top extends Module{
     io.ID_ALU_Data2 := inst_decode_unit.io.ID_to_EX_bus.bits.ALU_Data2
     io.ID_Rs1Data := inst_decode_unit.io.ID_to_EX_bus.bits.rs1_data
     io.ID_Rs2Data := inst_decode_unit.io.ID_to_EX_bus.bits.rs2_data
+    io.EX_ALU_result_pass := excute_unit.io.EX_ALUResult_pass
     io.ALUResult  := excute_unit.io.EX_to_MEM_bus.bits.ALU_result
     io.stall := inst_decode_unit.io.ID_stall
     
