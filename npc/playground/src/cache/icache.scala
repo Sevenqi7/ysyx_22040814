17f12e9efce5f460d2ccf09e36f8d4d7ecf0b198
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-15 21:04:26 +0800

    > generate verilog ysyx_22040814 蒋晓天 Linux seven7 6.3.1-1-default #1 SMP PREEMPT_DYNAMIC Wed May  3 05:18:39 UTC 2023 (8a31779) x86_64 x86_64 x86_64 GNU/Linux  21:04:26  已启动  1:02， 6 个用户， 平均负载：2.01, 2.00, 1.60

--- npc/playground/src/cache/icache.scala
+++ npc/playground/src/cache/icache.scala
@@ -72,7 +72,7 @@ class ICache(tagWidth: Int, nrSets: Int, nrLines: Int, offsetWidth: Int) extends
                     when(cache(set)(i).tag === tag && cache(set)(i).valid){
                     io.hit      := 1.U
                     io.rvalid   := 1.U
-                    io.rdata    := cache(set)(i).data(dataWidth - 1 - offset.asSInt * 8.S, dataWidth - 32 - offset.asSInt * 8)
+                    io.rdata    := cache(set)(i).data(dataWidth - 1 - offset.asInt * 8, dataWidth - 32 - offset.asInt * 8)
                 }
             }    
             when(!io.hit){
