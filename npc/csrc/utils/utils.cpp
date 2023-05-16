f25afd1131b888e666e691a4cb549971dad6aed4
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-16 16:47:30 +0800

    > generate verilog ysyx_22040814 蒋晓天 Linux seven7 6.3.1-1-default #1 SMP PREEMPT_DYNAMIC Wed May  3 05:18:39 UTC 2023 (8a31779) x86_64 x86_64 x86_64 GNU/Linux  16:47:30  已启动  6:25， 5 个用户， 平均负载：1.38, 1.31, 1.14

--- npc/csrc/utils/utils.cpp
+++ npc/csrc/utils/utils.cpp
@@ -40,7 +40,7 @@ void clock_step()
     Log("cache_hit:%d", top->io_cache_hit);
     Log("cache_state:%d", top->io_cache_state);
     Log("cache_axi_req:%d", top->io_cache_axi_req);
-    Log("cache_rvalid:%d", top->io_cache_valid);
+    Log("cache_rvalid:%d", top->io_cache_rvalid);
     Log("IF_req:%d", top->io_IF_AXIREQ);
     Log("IF_Inst:0x%x", top->io_IF_Inst);
     Log("ID_pc:0x%lx", top->io_ID_pc);
