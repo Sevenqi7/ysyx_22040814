6f7c3f7a6b584619f8852f7487a5190c33e272bc
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-18 21:14:09 +0800

    > generate verilog ysyx_22040814 蒋晓天 Linux seven7 6.3.1-2-default #1 SMP PREEMPT_DYNAMIC Tue May  9 07:50:57 UTC 2023 (78ee477) x86_64 x86_64 x86_64 GNU/Linux  21:14:09  已启动  9:29， 6 个用户， 平均负载：2.01, 1.57, 1.05

--- npc/playground/src/pipeline/inst_fetch/IFU.scala
+++ npc/playground/src/pipeline/inst_fetch/IFU.scala
@@ -65,8 +65,9 @@ class IFU extends Module{
     val IF_valid = Wire(Bool())
 
     IF_pc := MuxCase(pre_fetch.io.PF_pc, Seq(
+                (!IF_Inst              , 0.U),
                 (!io.IF_to_ID_bus.ready, io.IF_to_ID_bus.bits.PC),
-                (io.bp_flush | !IF_valid                 , 0.U)
+                (io.bp_flush |         , 0.U)
             ))
 
     IF_Inst := MuxCase(pre_fetch.io.inst, Seq(
