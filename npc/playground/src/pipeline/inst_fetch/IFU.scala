776444d095ef1b0f21bc98f19205f9ccd3f33067
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-18 18:41:36 +0800

    > generate verilog ysyx_22040814 蒋晓天 Linux seven7 6.3.1-2-default #1 SMP PREEMPT_DYNAMIC Tue May  9 07:50:57 UTC 2023 (78ee477) x86_64 x86_64 x86_64 GNU/Linux  18:41:36  已启动  6:57， 6 个用户， 平均负载：2.42, 1.06, 0.77

--- npc/playground/src/pipeline/inst_fetch/IFU.scala
+++ npc/playground/src/pipeline/inst_fetch/IFU.scala
@@ -66,12 +66,11 @@ class IFU extends Module{
         regConnect(io.IF_to_ID_bus.bits.Inst        , pre_fetch.io.inst         )
     }.otherwise{
         regConnect(io.IF_to_ID_bus.valid        , pre_fetch.io.PF_pc            )
-        regConnect(io.IF_to_ID_bus.bits.PC      , pre_fetch.io.PF_pc            )
-        regConnect(io.IF_to_ID_bus.bits.Inst    , pre_fetch.io.PF_pc            )
+        regConnect(io.IF_to_ID_bus.bits.PC      , pre_fetch.io.inst             )
+        regConnect(io.IF_to_ID_bus.bits.Inst    , pre_fetch.io.inst_valid       )
     }
     // regConnectWithResetAndStall(io.IF_to_ID_bus.bits.PC, pre_fetch.io.PF_pc   , flush, 0.U, !io.IF_to_ID_bus.ready)
     // regConnectWithResetAndStall(io.IF_to_ID_bus.valid, pre_fetch.io.inst_valid, flush, 0.U, !io.IF_to_ID_bus.ready)
-    // regConnectWithResetAndStall(io.IF_to_ID_bus.bits.Inst, pre_fetch.io.inst  , flush, 0.U, !io.IF_to_ID_bus.ready)
     // regConnectWithResetAndStall(io.IF_to_ID_bus.bits.Inst, pre_fetch.io.inst  , flush, 0.U, !io.IF_to_ID_bus.ready)
 
 }
