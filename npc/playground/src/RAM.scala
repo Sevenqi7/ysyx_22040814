a48e00b4be803f3aba29c46cca51edf93e873708
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-20 22:01:54 +0800

    > generate verilog ysyx_22040814 蒋晓天 Linux seven7 6.3.1-2-default #1 SMP PREEMPT_DYNAMIC Tue May  9 07:50:57 UTC 2023 (78ee477) x86_64 x86_64 x86_64 GNU/Linux  22:01:54  已启动  0:27， 4 个用户， 平均负载：0.40, 0.28, 0.28

--- npc/playground/src/RAM.scala
+++ npc/playground/src/RAM.scala
@@ -59,7 +59,6 @@ class AXI_Arbiter(val n: Int) extends Module{
     val out = IO(new AXIMasterIF(32, 64, 4))
 
 
-
     out <> in(n-1)
     for(i <- n - 1 to 0 by -1){
         req(i).ready                := 0.U
@@ -74,7 +73,7 @@ class AXI_Arbiter(val n: Int) extends Module{
         in(i).writeResp.bits.id     := 0.U
         in(i).writeResp.bits.resp   := 0.U
         in(i).writeResp.valid       := 0.U
-        when(req(i).valid & out.readAddr.ready & out.writeAddr.ready){             
+        when(req(i).valid){             
             out <> in(i)
             req(i).ready := 1.U
             for(j <- i+1 to n-1){
