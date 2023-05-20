33a47fc2b10991c532ad2e87b2ece9a3d38c9ed5
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-20 21:15:50 +0800

    > compile NEMU ysyx_22040814 蒋晓天 Linux seven7 6.3.2-1-default #1 SMP PREEMPT_DYNAMIC Mon May 15 15:59:38 UTC 2023 (70ea6f6) x86_64 x86_64 x86_64 GNU/Linux  21:15:50  已启动  1:20， 5 个用户， 平均负载：1.08, 1.34, 1.27

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -31,7 +31,7 @@ OBJS = $(SRCS:%.c=$(OBJ_DIR)/%.o) $(CXXSRC:%.cc=$(OBJ_DIR)/%.o)
 $(OBJ_DIR)/%.o: %.c
 	@echo + CC $<
 	@mkdir -p $(dir $@)
-	@$(CC) $(CFLAGS) -c -o $@ $< -fPIE -fPIC
+	@$(CC) $(CFLAGS) -c -o $@ $< -fPIE
 	$(call call_fixdep, $(@:.o=.d), $@)
 
 $(OBJ_DIR)/%.o: %.cc
