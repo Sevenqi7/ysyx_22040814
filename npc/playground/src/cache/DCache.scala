ad9a473bb05d07adf332dd4db8101723ceb64c21
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-06 18:27:58 +0800

    > generate verilog ysyx_22040814 蒋晓天 Linux seven7 6.3.4-1-default #1 SMP PREEMPT_DYNAMIC Mon May 29 06:22:29 UTC 2023 (2c66b1f) x86_64 x86_64 x86_64 GNU/Linux  18:27:58  已启动  7:20， 5 个用户， 平均负载：1.60, 1.24, 1.25

--- npc/playground/src/cache/DCache.scala
+++ npc/playground/src/cache/DCache.scala
@@ -251,7 +251,7 @@ class DCache (tagWidth: Int, nrSets: Int, nrLines: Int, offsetWidth: Int) extend
 
             dataMask                := (dataMask << (req_woffset(2, 0) << 3.U))
             maskedData              := (req_wdata << (req_woffset(2, 0) << 3.U)) & dataMask
-            when((req_woffet & "b1000".U) > 0.U){
+            when((req_woffset & "b1000".U) > 0.U){
                 cache(req_wset)(req_wline).data   := Cat(cache(req_wset)(req_wline).data(127, 64), 
                                                          cache(req_wset)(req_wline).data & ~dataMask | maskedData)
             }
