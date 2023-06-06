752b1801c9e4dda6543282f734de17443a7fd14f
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-06 21:53:11 +0800

    > generate verilog ysyx_22040814 蒋晓天 Linux seven7 6.3.4-1-default #1 SMP PREEMPT_DYNAMIC Mon May 29 06:22:29 UTC 2023 (2c66b1f) x86_64 x86_64 x86_64 GNU/Linux  21:53:11  已启动 10:45， 5 个用户， 平均负载：1.31, 1.41, 1.27

--- npc/csrc/utils/utils.cpp
+++ npc/csrc/utils/utils.cpp
@@ -14,24 +14,24 @@ void clock_step()
 
 #ifdef CONFIG_DEBUGMSG
     printf("\n");
-    Log("bp_npc:0x%lx", top->io_bp_npc);
-    Log("bp_taken:%d", top->io_bp_taken);
-    Log("bp_flush:%d", top->io_bp_flush);
-    Log("BTB_hit:%d", top->io_BTB_hit);
-    Log("ras_push:0x%lx", top->io_ras_push);
-    Log("ras_pop:0x%lx", top->io_ras_pop);
+    // Log("bp_npc:0x%lx", top->io_bp_npc);
+    // Log("bp_taken:%d", top->io_bp_taken);
+    // Log("bp_flush:%d", top->io_bp_flush);
+    // Log("BTB_hit:%d", top->io_BTB_hit);
+    // Log("ras_push:0x%lx", top->io_ras_push);
+    // Log("ras_pop:0x%lx", top->io_ras_pop);
     // Log("bht_update:%d", top->io_bht_update);
     // Log("pht_update:%d", top->io_pht_update);
     // Log("pht_idx:%d", top->io_pht_idx);
     // Log("pht_sel:%d", top->io_pht_sel);
     // Log("total fail prediction: %ld", top->io_jal_fail + top->io_jalr_fail + top->io_btype_fail);
 
-    Log("BTB_rtag:0x%x", top->io_BTB_rtag);
-    Log("BTB_rset:0x%x", top->io_BTB_rset);
-    Log("BTB_rdata:0x%lx", top->io_BTB_rdata);
-    Log("BTB_wtag:0x%x", top->io_BTB_wtag);
-    Log("BTB_wset:0x%x", top->io_BTB_wset);
-    Log("BTB_wdata:0x%lx", top->io_BTB_wdata);
+    // Log("BTB_rtag:0x%x", top->io_BTB_rtag);
+    // Log("BTB_rset:0x%x", top->io_BTB_rset);
+    // Log("BTB_rdata:0x%lx", top->io_BTB_rdata);
+    // Log("BTB_wtag:0x%x", top->io_BTB_wtag);
+    // Log("BTB_wset:0x%x", top->io_BTB_wset);
+    // Log("BTB_wdata:0x%lx", top->io_BTB_wdata);
     // Log("ID_npc:0x%lx", top->io_ID_npc);
     // Log("PF_npc:0x%lx", top->io_PF_npc);
     Log("PF_pc:0x%lx", top->io_PF_pc);
