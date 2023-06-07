83a221206ff42262d1dbc6edcac58a6cfce32870
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-07 12:35:54 +0800

    > generate verilog ysyx_22040814 蒋晓天 Linux seven7 6.3.4-1-default #1 SMP PREEMPT_DYNAMIC Mon May 29 06:22:29 UTC 2023 (2c66b1f) x86_64 x86_64 x86_64 GNU/Linux  12:35:54  已启动  0:13， 3 个用户， 平均负载：0.58, 1.08, 0.70

--- npc/playground/verilog/sim_sram.v
+++ npc/playground/verilog/sim_sram.v
@@ -137,26 +137,32 @@ module sim_sram(
     
     always@(posedge aclk) begin
         if(!aresetn) begin
-            rvalid_r = 1'b0;
-            rresp_r  = 2'b0;
+            rvalid_r <= 1'b0;
+            rresp_r  <= 2'b0;
         end
         else begin
             if(arvalid && !arv_arr_flag) begin
-                dci_pmem_read({32'b0, araddr}, rdata, 8'HFF);
-                rvalid_r    = 1'b1;
-                rresp_r     = 2'b0;
+                rvalid_r    <= 1'b1;
+                rresp_r     <= 2'b0;
             end
             else if(arv_arr_flag) begin
-                dci_pmem_read({32'b0, araddr_r}, rdata, 8'HFF);
-                rvalid_r    = 1'b1;
-                rresp_r     = 2'b0;
+                rvalid_r    <= 1'b1;
+                rresp_r     <= 2'b0;
             end
             else if(rvalid_r && rready) begin
-                rvalid_r    = 1'b0;
+                rvalid_r    <= 1'b0;
             end
         end
     end
 
+    always@(posedge aclk) begin
+        if(arvalid & !arv_arr_flag) begin
+            dci_pmem_read({32'b0, araddr}, rdata, 8'HFF);
+        end
+        else if(arv_arr_flag) begin
+            dci_pmem_read({32'b0, araddr_r}, rdata, 8'HFF);
+        end
+    end
 
     //aw
     always@(posedge aclk) begin
